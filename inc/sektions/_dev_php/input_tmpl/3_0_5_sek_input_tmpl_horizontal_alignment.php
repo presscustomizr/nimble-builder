4d927e2015ec43babbb1cbc82b1d71bdd7b46758
nikeo <nicolas@presscustomizr.com>
2018-05-17 22:02:25 +0200

    cleaned up code

--- inc/sektions/_dev_php/input_tmpl/3_0_5_sek_input_tmpl_horizontal_alignment.php
+++ inc/sektions/_dev_php/input_tmpl/3_0_5_sek_input_tmpl_horizontal_alignment.php
@@ -16,9 +16,6 @@ function sek_set_input_tmpl___h_alignment( $input_id, $input_data ) {
             <div data-sek-align="left" title="<?php _e('Align left','text_domain_to_be_translated'); ?>"><i class="material-icons">format_align_left</i></div>
             <div data-sek-align="center" title="<?php _e('Align center','text_domain_to_be_translated'); ?>"><i class="material-icons">format_align_center</i></div>
             <div data-sek-align="right" title="<?php _e('Align right','text_domain_to_be_translated'); ?>"><i class="material-icons">format_align_right</i></div>
-            <?php if ( 'czr_set_input_tmpl___h_text_alignment' == current_filter() ) : ?>
-              <div data-sek-align="justify" title="<?php _e('Justified','text_domain_to_be_translated'); ?>"><i class="material-icons">format_align_justify</i></div>
-            <?php endif; ?>
           </div>
         </div><?php // sek-h-align-wrapper ?>
     <?php
