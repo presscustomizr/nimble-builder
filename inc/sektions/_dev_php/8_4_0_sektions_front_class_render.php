440cd92f10c1dcd5c681d827a32134b54a0f6b2c
nikeo <nicolas@presscustomizr.com>
2018-06-05 23:42:58 +0200

    incorporate the utility sek_print_tiny_mce_text_content() inside the tmpl instead of externalizing it in the SEK_Front class

--- inc/sektions/_dev_php/8_4_0_sektions_front_class_render.php
+++ inc/sektions/_dev_php/8_4_0_sektions_front_class_render.php
@@ -284,23 +284,6 @@ if ( ! class_exists( 'SEK_Front_Render' ) ) :
                 return $ph;
             }
         }
-
-
-        // Utility to print the text content generated with tinyMce
-        // should be wrapped in a specific selector when customizing,
-        //  => so we can listen to user click actions and open the editor on for each separate tiny_mce_editor input
-        function sek_print_tiny_mce_text_content( $tiny_mce_content, $input_id, $module_model ) {
-            if ( empty( $tiny_mce_content ) ) {
-                echo $this -> sek_get_input_placeholder_content( 'tiny_mce_editor', $input_id );
-            } else {
-                $content = apply_filters( 'the_content', $tiny_mce_content );
-                if ( skp_is_customizing() ) {
-                    printf('<div title="%3$s" data-sek-input-type="tiny_mce_editor" data-sek-input-id="%1$s">%2$s</div>', $input_id, $content, __('Click to edit', 'here') );
-                } else {
-                    echo $content;
-                }
-            }
-        }
     }//class
 endif;
 ?>
--- tmpl/modules/tinymce_editor_module_tmpl.php
+++ tmpl/modules/tinymce_editor_module_tmpl.php
@@ -5,15 +5,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 $model = SEK_Front() -> model;
 $module_type = $model['module_type'];
-// print the module content if not empty
-if ( ! array_key_exists( 'value', $model ) ) {
-    // print the placeholder for each input_types
-    $tmpl_data = sek_get_registered_module_type_property( $module_type, 'tmpl' );
-    // foreach( $tmpl_data['item-inputs'] as $input_id => $input_data ) {
-    //     echo SEK_Front() -> sek_get_input_placeholder_content( $input_data['input_type'], $input_id );
-    // }
-} else {
-    if ( array_key_exists('content', $model['value'] ) ) {
-        SEK_Front() -> sek_print_tiny_mce_text_content( $model['value']['content'], 'content', $model );
+$value = array_key_exists( 'value', $model ) ? $model['value'] : array();
+
+// Utility to print the text content generated with tinyMce
+// should be wrapped in a specific selector when customizing,
+//  => so we can listen to user click actions and open the editor on for each separate tiny_mce_editor input
+if ( ! function_exists( '\Nimble\sek_print_tiny_mce_text_content') ) {
+    function sek_print_tiny_mce_text_content( $tiny_mce_content, $input_id, $module_model ) {
+        if ( empty( $tiny_mce_content ) ) {
+            echo SEK_Front()->sek_get_input_placeholder_content( 'tiny_mce_editor', $input_id );
+        } else {
+            $content = apply_filters( 'the_content', $tiny_mce_content );
+            if ( skp_is_customizing() ) {
+                printf('<div title="%3$s" data-sek-input-type="tiny_mce_editor" data-sek-input-id="%1$s">%2$s</div>', $input_id, $content, __('Click to edit', 'here') );
+            } else {
+                echo $content;
+            }
+        }
     }
+}
+// print the module content if not empty
+if ( array_key_exists('content', $value ) ) {
+    sek_print_tiny_mce_text_content( $value['content'], 'content', $model );
 }
