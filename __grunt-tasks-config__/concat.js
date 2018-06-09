ff114e6da10ce962f72d5afda3acc05a6e886131
nikeo <nicolas@presscustomizr.com>
2018-06-09 17:40:51 +0200

    improved : dev php file structure of the module registration split between ui and front modules. To have a parity with the way js files are organized.

--- __grunt-tasks-config__/concat.js
+++ __grunt-tasks-config__/concat.js
@@ -193,16 +193,18 @@ module.exports = {
 
       // MODULE REGISTRATION
       '<%= paths.sektions %>_dev_php/module_registration/4_0_0_sek_register_modules_after_setup_theme_50.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_1_sek_register_module_picker.php',
-      //'<%= paths.sektions %>_dev_php/module_registration/4_0_2_sek_register_section_picker.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_3_sek_register_background_border.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_3_sek_register_section_layout.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_3_sek_register_height.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_4_sek_register_spacing.php',
-      //'<%= paths.sektions %>_dev_php/module_registration/4_0_5_sek_register_simple_html.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_6_sek_register_tiny_mce_editor.php',
-      '<%= paths.sektions %>_dev_php/module_registration/4_0_7_sek_register_image.php',
-      //'<%= paths.sektions %>_dev_php/module_registration/4_0_8_sek_register_featured_pages.php',
+
+      '<%= paths.sektions %>_dev_php/module_registration/ui/4_0_1_sek_register_module_picker.php',
+      //'<%= paths.sektions %>_dev_php/module_registration/ui/4_0_2_sek_register_section_picker.php',
+      '<%= paths.sektions %>_dev_php/module_registration/ui/4_0_3_sek_register_background_border.php',
+      '<%= paths.sektions %>_dev_php/module_registration/ui/4_0_3_sek_register_section_layout.php',
+      '<%= paths.sektions %>_dev_php/module_registration/ui/4_0_3_sek_register_height.php',
+      '<%= paths.sektions %>_dev_php/module_registration/ui/4_0_4_sek_register_spacing.php',
+
+      //'<%= paths.sektions %>_dev_php/module_registration/front/4_0_5_sek_register_simple_html.php',
+      '<%= paths.sektions %>_dev_php/module_registration/front/4_0_6_sek_register_tiny_mce_editor.php',
+      '<%= paths.sektions %>_dev_php/module_registration/front/4_0_7_sek_register_image.php',
+      //'<%= paths.sektions %>_dev_php/module_registration/front/4_0_8_sek_register_featured_pages.php',
 
       // DYN CSS
       '<%= paths.sektions %>_dev_php/dyn_css_builder_and_google_fonts_printer/5_0_1_class-sek-dyn-css-builder.php',
