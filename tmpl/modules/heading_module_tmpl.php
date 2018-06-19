c0bcd41cf2701aedbc7d7469d9025f4b08100a24
Rocco Aliberti <raliberti84@gmail.com>
2018-06-19 11:18:25 +0200

    Imp: create specific heading text render function

--- tmpl/modules/heading_module_tmpl.php
+++ tmpl/modules/heading_module_tmpl.php
@@ -10,20 +10,32 @@ $value = array_key_exists( 'value', $model ) ? $model['value'] : array();
 // Utility to print the text content generated with tinyMce
 // should be wrapped in a specific selector when customizing,
 //  => so we can listen to user click actions and open the editor on for each separate tiny_mce_editor input
-if ( ! function_exists( '\Nimble\sek_print_tiny_mce_text_content') ) {
-    function sek_print_tiny_mce_text_content( $tiny_mce_content, $input_id, $module_model, $strip_tags = false, $allowed_tags = '' ) {
+if ( ! function_exists( '\Nimble\sek_print_tiny_mce_text_heading_content') ) {
+    function sek_print_tiny_mce_text_heading_content( $tiny_mce_content, $input_id, $module_model, $args = array() ) {
+        $defaults = array(
+            'strip_tags'   => true,
+            'allowed_tags' => '<br><a><span><img><strong><em>',
+            'echo'         => false
+        );
+        $args = wp_parse_args( $args, $defaults );
+
         if ( empty( $tiny_mce_content ) ) {
-            echo SEK_Front()->sek_get_input_placeholder_content( 'tiny_mce_editor', $input_id );
+            $to_print = SEK_Front()->sek_get_input_placeholder_content( 'tiny_mce_editor', $input_id );
         } else {
             $content = apply_filters( 'the_content', $tiny_mce_content );
-            if ( $strip_tags ) {
-                $content = strip_tags( $content, $allowed_tags );
+            if ( $args[ 'strip_tags' ] ) {
+                $content = strip_tags( $content, $args[ 'allowed_tags' ] );
             }
             if ( skp_is_customizing() ) {
-                printf('<div title="%3$s" data-sek-input-type="tiny_mce_editor" data-sek-input-id="%1$s">%2$s</div>', $input_id, $content, __('Click to edit', 'here') );
+                $to_print = sprintf('<div title="%3$s" data-sek-input-type="tiny_mce_editor" data-sek-input-id="%1$s">%2$s</div>', $input_id, $content, __('Click to edit', 'here') );
             } else {
-                echo $content;
+                $to_print = $content;
             }
+        }
+        if ( $args[ 'echo' ] ) {
+            echo $to_print;
+        } else {
+            return $to_print;
         }
     }
 }
@@ -31,7 +43,8 @@ if ( ! function_exists( '\Nimble\sek_print_tiny_mce_text_content') ) {
 if ( array_key_exists('heading_text', $value ) ) {
     $tag = empty( $value[ 'heading_tag' ] ) ? 'h1' : $value[ 'heading_tag' ];
 
-    echo "<$tag>";
-    sek_print_tiny_mce_text_content( $value['heading_text'], 'heading_text', $model, $strip_tags = true, $allowed_tags = '<br><a><span><img><strong><em>' );
-    echo "</$tag>";
+    printf( '<%1$s>%2$s</%1$s>',
+        $tag,
+        sek_print_tiny_mce_text_heading_content( $value['heading_text'], 'heading_text', $model )
+    );
 }
