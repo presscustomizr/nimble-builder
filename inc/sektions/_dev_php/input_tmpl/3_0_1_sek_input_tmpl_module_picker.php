a74338833a56e228d61e1f7ce73c93a2ffd5fdbc
nikeo <nicolas@presscustomizr.com>
2018-07-02 14:50:02 +0200

    added new set of svg icons for the module picker

--- inc/sektions/_dev_php/input_tmpl/3_0_1_sek_input_tmpl_module_picker.php
+++ inc/sektions/_dev_php/input_tmpl/3_0_1_sek_input_tmpl_module_picker.php
@@ -13,36 +13,37 @@ function sek_set_input_tmpl___module_picker( $input_id, $input_data ) {
                   'content-type' => 'module',
                   'content-id' => 'czr_tiny_mce_editor_module',
                   'title' => __( 'Text Editor', 'text_domain_to_be_replaced' ),
-                  'icon' => 'short_text'
+                  'icon' => 'Nimble__text_icon.svg'
                 ),
                 array(
                   'content-type' => 'module',
                   'content-id' => 'czr_image_module',
                   'title' => __( 'Image', 'text_domain_to_be_replaced' ),
-                  'icon' => 'image'
+                  'icon' => 'Nimble__image_icon.svg'
                 ),
                 array(
                   'content-type' => 'module',
                   'content-id' => 'czr_heading_module',
                   'title' => __( 'Heading', 'text_domain_to_be_replaced' ),
-                  'icon' => 'title'
+                  'icon' => 'Nimble__heading_icon.svg'
                 ),
                 array(
                   'content-type' => 'module',
                   'content-id' => 'czr_spacer_module',
                   'title' => __( 'Spacer', 'text_domain_to_be_replaced' ),
-                  'icon' => 'unfold_more'
+                  'icon' => 'Nimble__spacer_icon.svg'
                 ),
                 array(
                   'content-type' => 'module',
                   'content-id' => 'czr_divider_module',
                   'title' => __( 'Divider', 'text_domain_to_be_replaced' ),
-                  'icon' => 'unfold_more'
+                  'icon' => 'Nimble__divider_icon.svg'
                 ),
                 array(
                   'content-type' => 'module',
                   'content-id' => 'czr_icon_module',
                   'title' => __( 'Icon', 'text_domain_to_be_replaced' ),
+                  'icon' => 'Nimble__icon_icon.svg'
                 )
                 // array(
                 //   'content-type' => 'module',
@@ -61,10 +62,11 @@ function sek_set_input_tmpl___module_picker( $input_id, $input_data ) {
                 if ( $i % 2 == 0 ) {
                   //printf('<div class="sek-module-raw"></div');
                 }
+                $icon_img_src = NIMBLE_BASE_URL . '/assets/czr/sek/icons/modules/' . $_params['icon'];
                 printf('<div draggable="true" data-sek-content-type="%1$s" data-sek-content-id="%2$s" title="%5$s"><span class="sek-module-icon">%3$s</span><span class="sek-module-title">%4$s</span></div>',
                     $_params['content-type'],
                     $_params['content-id'],
-                    '<i class="material-icons">' . $_params['icon'] .'</i>',
+                    '<img title="'. $_params['title'] . '" alt="'. $_params['title'] . '" class="nimble-module-icons" src="' . $icon_img_src .'"/>',
                     $_params['title'],
                     __('Drag and drop the module in the previewed page.', 'text_domain_to_be_replaced' )
                 );
