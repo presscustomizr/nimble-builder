4eae56360de7f4f2d95efb83c0623b512e79f6dc
nikeo <nicolas@presscustomizr.com>
2018-07-05 14:19:43 +0200

    fixed typo in the comments

--- inc/sektions/_dev_php/8_4_0_sektions_front_class_render.php
+++ inc/sektions/_dev_php/8_4_0_sektions_front_class_render.php
@@ -7,7 +7,7 @@ if ( ! class_exists( 'SEK_Front_Render' ) ) :
             if ( !defined( "NIMBLE_AFTER_CONTENT_FILTER_PRIORITY" ) ) { define( "NIMBLE_AFTER_CONTENT_FILTER_PRIORITY", PHP_INT_MAX ); }
             if ( !defined( "NIMBLE_WP_CONTENT_WRAP_FILTER_PRIORITY" ) ) { define( "NIMBLE_WP_CONTENT_WRAP_FILTER_PRIORITY", - PHP_INT_MAX ); }
 
-            // SCHEDULE THE ACITONS ON HOOKS AND CONTENT FILTERS
+            // SCHEDULE THE ACTIONS ON HOOKS AND CONTENT FILTERS
             foreach( sek_get_locations() as $hook ) {
                 switch ( $hook ) {
                     case 'loop_start' :
