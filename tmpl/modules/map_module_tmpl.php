9d85624e22f2ee70d7e876bb7e1448c9dfb2a827
Rocco Aliberti <raliberti84@gmail.com>
2018-07-11 18:02:03 +0200

    Add: add map module prototype fixes #76

---
+++ tmpl/modules/map_module_tmpl.php
@@ -0,0 +1,23 @@
+<?php
+namespace Nimble;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+$model = SEK_Front() -> model;
+$value = array_key_exists( 'value', $model ) ? $model['value'] : array();
+
+
+if ( empty( $value['address'] ) ) {
+    return;
+}
+
+if ( 0 === absint( $value['zoom'] ) ) {
+    $value['zoom'] = 10;
+}
+
+printf(
+    '<div class="sek-embed"><iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=%s&amp;t=m&amp;z=%d&amp;output=embed&amp;iwloc=near" aria-label="%s"></iframe></div>',
+    rawurlencode( $value['address'] ),
+    absint( $value['zoom'] ),
+    esc_attr( $value['address'] )
+);
