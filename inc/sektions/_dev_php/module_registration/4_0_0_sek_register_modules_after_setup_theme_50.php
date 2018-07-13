2623e8e1201b57c09baa5dbd446ba05365fe574b
nikeo <nicolas@presscustomizr.com>
2018-07-14 01:07:02 +0200

    added a utility sek_get_select_options_for_input_id() needed for #113

--- inc/sektions/_dev_php/module_registration/4_0_0_sek_register_modules_after_setup_theme_50.php
+++ inc/sektions/_dev_php/module_registration/4_0_0_sek_register_modules_after_setup_theme_50.php
@@ -35,4 +35,160 @@ function sek_register_modules() {
 
 }//sek_register_modules()
 
+
+// HELPERS
+// Used when registrating a select input in a module
+// @return an array of options that will be used to populate the select input in js
+function sek_get_select_options_for_input_id( $input_id ) {
+    $options = array();
+    switch( $input_id ) {
+        // IMAGE MODULE
+        case 'link-to' :
+            $options = array(
+                'no-link' => __('No link', 'text_domain_to_be_replaced' ),
+                'url' => __('Site content or custom url', 'text_domain_to_be_replaced' ),
+                'img-file' => __('Image file', 'text_domain_to_be_replaced' ),
+                'img-page' =>__('Image page', 'text_domain_to_be_replaced' )
+            );
+        break;
+        case 'img-size' :
+            $options = sek_get_img_sizes();
+        break;
+
+        // FEATURED PAGE MODULE
+        case 'img-type' :
+            $options = array(
+                'none' => __('No text', 'text_domain_to_be_replaced' ),
+                'page-excerpt' => __('Use the page excerpt', 'text_domain_to_be_replaced' ),
+                'custom' => __('Use a custom text', 'text_domain_to_be_replaced' ),
+            );
+        break;
+        case 'content-type' :
+            $options = array(
+                'none' => __('No image', 'text_domain_to_be_replaced' ),
+                'featured' => __('Use the page featured image', 'text_domain_to_be_replaced' ),
+                'custom' => __('Use a custom image', 'text_domain_to_be_replaced' ),
+            );
+        break;
+
+        // HEADING MODULE
+        case 'heading_tag':
+            $options = array(
+                /* Not totally sure these should be localized as they strictly refer to html tags */
+                'h1' => __('H1', 'text_domain_to_be_replaced' ),
+                'h2' => __('H2', 'text_domain_to_be_replaced' ),
+                'h3' => __('H3', 'text_domain_to_be_replaced' ),
+                'h4' => __('H4', 'text_domain_to_be_replaced' ),
+                'h5' => __('H5', 'text_domain_to_be_replaced' ),
+                'h6' => __('H6', 'text_domain_to_be_replaced' ),
+            );
+        break;
+
+        // CSS MODIFIERS INPUT ID
+        case 'font_weight_css' :
+            $options = array(
+                'normal'  => __( 'normal', 'text_domain_to_be_replaced' ),
+                'bold'    => __( 'bold', 'text_domain_to_be_replaced' ),
+                'bolder'  => __( 'bolder', 'text_domain_to_be_replaced' ),
+                'lighter'   => __( 'lighter', 'text_domain_to_be_replaced' ),
+                100     => 100,
+                200     => 200,
+                300     => 300,
+                400     => 400,
+                500     => 500,
+                600     => 600,
+                700     => 700,
+                800     => 800,
+                900     => 900
+            );
+        break;
+        case 'font_style_css' :
+            $options = array(
+                'inherit'   => __( 'inherit', 'text_domain_to_be_replaced' ),
+                'italic'  => __( 'italic', 'text_domain_to_be_replaced' ),
+                'normal'  => __( 'normal', 'text_domain_to_be_replaced' ),
+                'oblique' => __( 'oblique', 'text_domain_to_be_replaced' )
+            );
+        break;
+        case 'text_decoration_css'  :
+            $options = array(
+                'none'      => __( 'none', 'text_domain_to_be_replaced' ),
+                'inherit'   => __( 'inherit', 'text_domain_to_be_replaced' ),
+                'line-through' => __( 'line-through', 'text_domain_to_be_replaced' ),
+                'overline'    => __( 'overline', 'text_domain_to_be_replaced' ),
+                'underline'   => __( 'underline', 'text_domain_to_be_replaced' )
+            );
+        break;
+        case 'text_transform_css' :
+            $options = array(
+                'none'      => __( 'none', 'text_domain_to_be_replaced' ),
+                'inherit'   => __( 'inherit', 'text_domain_to_be_replaced' ),
+                'capitalize'  => __( 'capitalize', 'text_domain_to_be_replaced' ),
+                'uppercase'   => __( 'uppercase', 'text_domain_to_be_replaced' ),
+                'lowercase'   => __( 'lowercase', 'text_domain_to_be_replaced' )
+            );
+        break;
+
+        // SPACING MODULE
+        case 'css_unit' :
+            $options = array(
+                'px' => __('Pixels', 'text_domain_to_be_replaced' ),
+                'em' => __('Em', 'text_domain_to_be_replaced'),
+                'percent' => __('Percents', 'text_domain_to_be_replaced' )
+            );
+        break;
+
+        //QUOTE MODULE
+        case 'quote_design' :
+            $options = array(
+                'none' => __( 'No design', 'text_domain_to_be_replaced' ),
+                'border-before' => __( 'Border on the left (right for RTL users)', 'text_domain_to_be_replaced' ),
+            );
+        break;
+
+        // LAYOUT BACKGROUND BORDER
+        case 'boxed-wide' :
+            $options = array(
+                'boxed' => __('Boxed', 'text_domain_to_be_replaced'),
+                'fullwidth' => __('Full Width', 'text_domain_to_be_replaced')
+            );
+        break;
+        case 'height-type' :
+            $options = array(
+                'default' => __('default', 'text_domain_to_be_replaced'),
+                'fit-to-screen' => __('Fit to screen', 'text_domain_to_be_replaced'),
+                'custom' => __('Custom', 'text_domain_to_be_replaced' )
+            );
+        break;
+        case 'bg-scale' :
+            $options = array(
+                'default' => __('default', 'text_domain_to_be_replaced'),
+                'auto' => __('auto', 'text_domain_to_be_replaced'),
+                'cover' => __('scale to fill', 'text_domain_to_be_replaced'),
+                'contain' => __('fit', 'text_domain_to_be_replaced'),
+            );
+        break;
+        case 'bg-position' :
+            $options = array(
+                'default' => __('default', 'text_domain_to_be_replaced'),
+            );
+        break;
+        case 'border-type' :
+            $options = array(
+                'none' => __('none', 'text_domain_to_be_replaced'),
+                'solid' => __('solid', 'text_domain_to_be_replaced'),
+                'double' => __('double', 'text_domain_to_be_replaced'),
+                'dotted' => __('dotted', 'text_domain_to_be_replaced'),
+                'dashed' => __('dashed', 'text_domain_to_be_replaced')
+            );
+        break;
+
+
+        default :
+            sek_error_log( __FUNCTION__ . ' => no case set for input id : '. $input_id );
+        break;
+    }
+    return $options;
+}
+
 ?>
