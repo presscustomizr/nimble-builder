410fd519ab0963f82a8331f154a741bc5eaf459f
nikeo <nicolas@presscustomizr.com>
2018-07-13 23:03:42 +0200

    added 'quote_design' => 'border-before' in the starting_value params

--- inc/sektions/_dev_php/module_registration/front/4_1_5_sek_register_quote.php
+++ inc/sektions/_dev_php/module_registration/front/4_1_5_sek_register_quote.php
@@ -37,7 +37,8 @@ function sek_get_module_params_for_czr_quote_module() {
         // 'validate_callback' => 'function_prefix_to_be_replaced_validate_callback__czr_social_module',
         'starting_value' => array(
             'quote_text'  => 'Hey, careful, man, there\'s a beverage here!',
-            'cite_text'   => 'The Dude in <a href="https://www.imdb.com/title/tt0118715/quotes/qt0464770" rel="nofollow noopener noreferrer" target="_blank">The Big Lebowski</a>'
+            'cite_text'   => 'The Dude in <a href="https://www.imdb.com/title/tt0118715/quotes/qt0464770" rel="nofollow noopener noreferrer" target="_blank">The Big Lebowski</a>',
+            'quote_design' => 'border-before'
         ),
         'css_selectors' => array( '.sek-module-inner' ),
         'tmpl' => array(
