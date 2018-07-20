d8b3953f30a355f0b2ca921e508b8bab3aa68ce9
nikeo <nicolas@presscustomizr.com>
2018-07-20 17:37:25 +0200

    added a simple range input. without the unit picker.

---
+++ assets/czr/sek/js/_dev_control/inputs/_7_0_8_range_simple.js
@@ -0,0 +1,33 @@
+//global sektionsLocalizedData
+( function ( api, $, _ ) {
+      // all available input type as a map
+      api.czrInputMap = api.czrInputMap || {};
+
+      // input_type => callback fn to fire in the Input constructor on initialize
+      // the callback can receive specific params define in each module constructor
+      // For example, a content picker can be given params to display only taxonomies
+      // the default input_event_map can also be overriden in this callback
+      $.extend( api.czrInputMap, {
+            range_simple : function( params ) {
+                  var input = this,
+                      $wrapper = $('.sek-range-with-unit-picker-wrapper', input.container ),
+                      $numberInput = $wrapper.find( 'input[type="number"]'),
+                      $rangeInput = $wrapper.find( 'input[type="range"]');
+
+                  // synchronizes range input and number input
+                  // number is the master => sets the input() val
+                  $rangeInput.on('input', function( evt ) {
+                        $numberInput.val( $(this).val() ).trigger('input');
+                  });
+                  $numberInput.on('input', function( evt ) {
+                        input( $(this).val() );
+                        $rangeInput.val( $(this).val() );
+                  });
+                  // trigger a change on init to sync the range input
+                  $rangeInput.val( $numberInput.val() );
+            },
+
+      });//$.extend( api.czrInputMap, {})
+
+
+})( wp.customize, jQuery, _ );
---
+++ inc/sektions/_dev_php/input_tmpl/3_1_1_sek_input_tmpl_range_simple.php
@@ -0,0 +1,39 @@
+<?php
+/* ------------------------------------------------------------------------- *
+ *  CODE EDITOR INPUT TEMPLATE
+/* ------------------------------------------------------------------------- */
+// @fired from  sek_set_input_tmpl_content( $input_type, $input_id, $input_data )
+function sek_set_input_tmpl___range_simple( $input_id, $input_data ) {
+    ?>
+    <?php
+      // we save the int value + unit
+      // we want to keep only the numbers when printing the tmpl
+      // dev note : value.replace(/\D+/g, '') : ''; not working because remove "." which we might use for em for example
+    ?>
+    <#
+      var value = data['<?php echo $input_id; ?>'],
+          unit = data['<?php echo $input_id; ?>'];
+      value = _.isString( value ) ? value.replace(/px|em|%/g,'') : value;
+      unit = _.isString( unit ) ? unit.replace(/[0-9]|\.|,/g, '') : 'px';
+      unit = _.isEmpty( unit ) ? 'px' : unit;
+    #>
+    <div class="sek-range-with-unit-picker-wrapper sek-no-unit-picker">
+        <# //console.log( 'IN php::sek_set_input_tmpl___range_simple() => data range_slide => ', data ); #>
+        <div class="sek-range-wrapper">
+          <input data-czrtype="<?php echo $input_id; ?>" type="hidden" data-sek-unit="{{ unit }}"/>
+          <?php
+          printf( '<input class="sek-range-input" type="range" %1$s %2$s %3$s %4$s/>',
+            ! empty( $input_data['orientation'] ) ? 'data-orientation="'. $input_data['orientation'] .'"' : '',
+            ! empty( $input_data['unit'] ) ? 'data-unit="'. $input_data['unit'] .'"' : '',
+            ! empty( $input_data['min'] ) ? 'min="'. $input_data['min'] .'"' : 'min="0"',
+            ! empty( $input_data['max'] ) ? 'max="'. $input_data['max'] .'"' : ''
+          );
+          ?>
+        </div>
+        <div class="sek-number-wrapper">
+            <input class="sek-pm-input" value="{{ value }}" type="number"  >
+        </div>
+    </div><?php // sek-spacing-wrapper ?>
+  <?php
+}
+?>
