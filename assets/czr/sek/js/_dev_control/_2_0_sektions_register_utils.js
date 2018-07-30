e777ae855ef4a8dc343f9b73f5b7b60b2d2706c0
nikeo <nicolas@presscustomizr.com>
2018-07-30 15:55:48 +0200

    imp : allow cleaning of a single element when passing the api id

--- assets/czr/sek/js/_dev_control/_2_0_sektions_register_utils.js
+++ assets/czr/sek/js/_dev_control/_2_0_sektions_register_utils.js
@@ -3,16 +3,21 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
 (function ( api, $ ) {
       $.extend( CZRSeksPrototype, {
             //@return void()
-            //clean all registered control, section, panel tracked ids
-            //preserve the settings
-            //typically fired before updating the ui. @see ::generateUI()
-            cleanRegistered : function() {
+            // clean registered controls, sections, panels
+            // only elements that have a true "track" param on registration are populated in the registered() collection
+            // if the _id_ param is not specified, all registered controls, sections and panels are removed.
+            //
+            // preserve the settings => because this is where the customizer changeset of values is persisted before publishing
+            // typically fired before updating the ui. @see ::generateUI()
+            cleanRegistered : function( _id_ ) {
                   var self = this,
                       registered = $.extend( true, [], self.registered() || [] );
 
                   registered = _.filter( registered, function( _reg_ ) {
                         if ( 'setting' !== _reg_.what ) {
                               if ( api[ _reg_.what ].has( _reg_.id ) ) {
+                                    if ( ! _.isEmpty( _id_ ) && _reg_.id !== _id_ )
+                                      return;
                                     // fire an event before removal, can be used to clean some jQuery plugin instance for example
                                     if (  _.isFunction( api[ _reg_.what ]( _reg_.id ).trigger ) ) {//<= Section and Panel constructor are not extended with the Event class, that's why we check if this method exists
                                            self.trigger( 'sek-ui-pre-removal', { what : _reg_.what, id : _reg_.id } );
