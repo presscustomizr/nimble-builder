9dfb69825866503355b68ee441fe3647d5892aad
Rocco Aliberti <raliberti84@gmail.com>
2018-08-01 20:00:23 +0200

    Fix: make the quote module enqueue fa in front by default fixes #167

--- inc/sektions/_dev_php/module_registration/front/4_1_5_sek_register_quote.php
+++ inc/sektions/_dev_php/module_registration/front/4_1_5_sek_register_quote.php
@@ -395,6 +395,14 @@ function sek_get_module_params_for_czr_quote_module() {
             )
         ),
         'render_tmpl_path' => NIMBLE_BASE_PATH . "/tmpl/modules/quote_module_tmpl.php",
+        'front_assets' => array(
+              'czr-font-awesome' => array(
+                  'type' => 'css',
+                  //'handle' => 'czr-font-awesome',
+                  'src' => NIMBLE_BASE_URL . '/assets/front/fonts/css/fontawesome-all.min.css'
+                  //'deps' => array()
+              )
+        )
     );
 }
 
