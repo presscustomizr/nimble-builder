fcbbc5f3aadd2a2c2e039fd7606f8bdaf7ac207c
nikeo <nicolas@presscustomizr.com>
2018-09-14 11:02:52 +0200

    changed the simple form wrapper css class name

--- tmpl/modules/simple_form_module_tmpl.php
+++ tmpl/modules/simple_form_module_tmpl.php
@@ -11,6 +11,6 @@ $value = array_key_exists( 'value', $model ) ? $model['value'] : array();
 //sek_error_log('Simple form value', $value);
 
 ?>
-<div class="simple-form-wrapper">
-  <?php echo SEK_Front() -> get_simple_form_html( $value['form_fields'] ); ?>
+<div class="sek-simple-form-wrapper">
+  <?php echo SEK_Front() -> get_simple_form_html( $model ); ?>
 </div>
