c3ba689c3d5b8e70fbd08c5860a2034afc6841b9
nikeo <nicolas@presscustomizr.com>
2018-09-15 15:56:54 +0200

    fixed wrong default value for font_size_css input

--- inc/sektions/_dev_php/module_registration/front/4_0_6_sek_register_tiny_mce_editor.php
+++ inc/sektions/_dev_php/module_registration/front/4_0_6_sek_register_tiny_mce_editor.php
@@ -45,7 +45,7 @@ function sek_get_module_params_for_czr_tiny_mce_editor_module() {
                             'font_size_css'       => array(
                                 'input_type'  => 'range_with_unit_picker_device_switcher',
                                 'title'       => __( 'Font size', 'text_domain_to_be_replaced' ),
-                                'default'     => array( 'desktop' => array() ),
+                                'default'     => array( 'desktop' => '16px' ),
                                 'min' => 0,
                                 'max' => 100,
                                 'title_width' => 'width-100',
