67c14c292bd3cdfc3779579bede473c8d8ab3e1d
nikeo <nicolas@presscustomizr.com>
2018-09-17 22:08:29 +0200

    added inset and outset shadow css selectors and rules to the form fields

--- tmpl/modules/simple_form_module_tmpl.php
+++ tmpl/modules/simple_form_module_tmpl.php
@@ -9,8 +9,15 @@ $module_type = $model['module_type'];
 $value = array_key_exists( 'value', $model ) ? $model['value'] : array();
 
 //sek_error_log('Simple form value', $value);
-
+$visual_effect_class = '';
+//visual effect classes
+if ( true === sek_booleanize_checkbox_val( $value['fields_design']['use_outset_shadow'] ) ) {
+    $visual_effect_class .= 'use-outset-shadow';
+}
+if ( true === sek_booleanize_checkbox_val( $value['fields_design']['use_inset_shadow'] ) ) {
+    $visual_effect_class .= ' use-inset-shadow';
+}
 ?>
-<div class="sek-simple-form-wrapper">
+<div class="sek-simple-form-wrapper <?php echo $visual_effect_class; ?>">
   <?php echo SEK_Front() -> get_simple_form_html( $model ); ?>
 </div>
