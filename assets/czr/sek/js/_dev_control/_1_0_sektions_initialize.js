95f7a4e500b7fc6ef94272f17a460b0e93e92f62
nikeo <nicolas@presscustomizr.com>
2018-09-18 15:13:32 +0200

    cleaned duplicated code for topbar

--- assets/czr/sek/js/_dev_control/_1_0_sektions_initialize.js
+++ assets/czr/sek/js/_dev_control/_1_0_sektions_initialize.js
@@ -203,6 +203,8 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
 
 
 
+
+
             // Fired in initialize()
             registerAndSetupDefaultPanelSectionOptions : function() {
                   var self = this;
@@ -355,6 +357,9 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
 
 
 
+
+
+
             //@return void()
             // sektionsData is built server side :
             //array(
@@ -382,212 +387,6 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
                         api.errare('::setContextualCollectionSettingIdWhenSkopeSet() => missing setting_id');
                   }
                   self.sekCollectionSettingId( sektionsData.setting_id );
-            },
-
-
-
-
-
-
-
-
-            // TOP BAR
-            // fired in ::initialize()
-            toggleTopBar : function( visible ) {
-                  visible = _.isUndefined( visible ) ? true : visible;
-                  var self = this,
-                      _renderAndSetup = function() {
-                            $.when( self.renderAndSetupTopBarTmpl({}) ).done( function( $_el ) {
-                                  self.topBarContainer = $_el;
-                                  //display
-                                  _.delay( function() {
-                                      $('body').addClass('nimble-top-bar-visible');
-                                  }, 200 );
-                            });
-                      },
-                      _hide = function() {
-                            var dfd = $.Deferred();
-                            $('body').removeClass('nimble-top-bar-visible');
-                            if ( self.topBarContainer && self.topBarContainer.length ) {
-                                  //remove Dom element after slide up
-                                  _.delay( function() {
-                                        //self.topBarContainer.remove();
-                                        dfd.resolve();
-                                  }, 300 );
-                            } else {
-                                dfd.resolve();
-                            }
-                            return dfd.promise();
-                      };
-
-                  if ( visible ) {
-                        _renderAndSetup();
-                  } else {
-                        _hide().done( function() {
-                              self.topBarVisible( false );//should be already false
-                        });
-                  }
-            },
-
-
-            //@param = { }
-            renderAndSetupTopBarTmpl : function( params ) {
-                  if ( $( '#nimble-top-bar' ).length > 0 )
-                    return $( '#nimble-top-bar' );
-
-                  var self = this;
-
-                  try {
-                        _tmpl =  wp.template( 'nimble-top-bar' )( {} );
-                  } catch( er ) {
-                        api.errare( 'Error when parsing the the top note template', er );
-                        return false;
-                  }
-                  $('#customize-preview').after( $( _tmpl ) );
-
-                  // Attach click events
-                  $(  '[data-nimble-history]', '#nimble-top-bar' ).on( 'click', function(evt) {
-                        try { self.navigateHistory( $(this).data( 'nimble-history') ); } catch( er ) {
-                              api.errare( 'Error when firing self.navigateHistory', er );
-                        }
-                  });
-                  $(  '.sek-settings', '#nimble-top-bar' ).on( 'click', function(evt) {
-                        // Generate UI for the local skope options
-                        self.generateUI({ action : 'sek-generate-local-skope-options-ui'}).done( function() {
-                              api.control( self.getLocalSkopeOptionId(), function( _control_ ) {
-                                    _control_.focus();
-                              });
-                        });
-                  });
-                  return $( '#nimble-top-bar' );
-            },
-
-
-            /* HISTORY */
-            // @param direction = string 'undo', 'redo'
-            // @return void()
-            navigateHistory : function( direction ) {
-                  var self = this,
-                      historyLog = $.extend( true, [], self.historyLog() );
-                  // log model
-                  // {
-                  //       status : 'current', 'previous', 'future'
-                  //       value : {},
-                  //       action : 'sek-add-column'
-                  // }
-
-                  // UPDATE THE SETTING VALUE
-                  var previous,
-                      current,
-                      future,
-                      newHistoryLog = [],
-                      newSettingValue,
-                      previousSektionToRefresh,
-                      currentSektionToRefresh;
-
-                  _.each( historyLog, function( log ) {
-                        if ( ! _.isEmpty( newSettingValue ) ) {
-                              return;
-                        }
-                        switch( log.status ) {
-                              case 'previous' :
-                                    previous = log;
-                              break;
-                              case 'current' :
-                                    current = log;
-                              break;
-                              case 'future' :
-                                    future = log;
-                              break;
-                        }
-                        switch( direction ) {
-                              case 'undo' :
-                                    // the last previous is our new setting value
-                                    if ( ! _.isEmpty( current ) && ! _.isEmpty( previous ) ) {
-                                          newSettingValue = previous.value;
-                                          previousSektionToRefresh = current.sektionToRefresh;
-                                          currentSektionToRefresh = previous.sektionToRefresh;
-                                    }
-                              break;
-                              case 'redo' :
-                                    // the first future is our new setting value
-                                    if ( ! _.isEmpty( future ) ) {
-                                          newSettingValue = future.value;
-                                          previousSektionToRefresh = current.sektionToRefresh;
-                                          currentSektionToRefresh = future.sektionToRefresh;
-                                    }
-                              break;
-                        }
-                  });
-
-                  // set the new setting Value
-                  if( ! _.isUndefined( newSettingValue ) ) {
-                        api( self.sekCollectionSettingId() )( self.validateSettingValue( newSettingValue ), { navigatingHistoryLogs : true } );
-
-                        // If the information is available, refresh only the relevant sections
-                        // otherwise fallback on a full refresh
-                        var previewHasBeenRefreshed = false;
-
-                        // if ( ! _.isEmpty( previousSektionToRefresh ) ) {
-                        //       api.previewer.trigger( 'sek-refresh-level', {
-                        //             level : 'section',
-                        //             id : previousSektionToRefresh
-                        //       });
-                        // } else {
-                        //       api.previewer.refresh();
-                        //       previewHasBeenRefreshed = true;
-                        // }
-                        // if ( currentSektionToRefresh != previousSektionToRefresh ) {
-                        //     if ( ! _.isEmpty( currentSektionToRefresh ) ) {
-                        //           api.previewer.trigger( 'sek-refresh-level', {
-                        //                 level : 'section',
-                        //                 id : currentSektionToRefresh
-                        //           });
-                        //     } else if ( ! previewHasBeenRefreshed ) {
-                        //           api.previewer.refresh();
-                        //     }
-                        // }
-                        api.previewer.refresh();
-
-                        // Always make sure that the ui gets refreshed
-                        api.previewer.trigger( 'sek-pick-module', {});
-                  }
-
-                  // UPDATE THE HISTORY LOG
-                  var currentKey = _.findKey( historyLog, { status : 'current'} );
-                  currentKey = Number( currentKey );
-                  if ( ! _.isNumber( currentKey ) ) {
-                        api.errare( 'Error when navigating the history log, the current key should be a number');
-                        return;
-                  }
-
-                  _.each( historyLog, function( log, key ) {
-                        newLog = $.extend( true, {}, log );
-                        // cast keys to number so we can compare them
-                        key = Number( key );
-                        switch( direction ) {
-                              case 'undo' :
-                                    if ( 0 < currentKey ) {
-                                          if ( key === ( currentKey - 1 ) ) {
-                                                newLog.status = 'current';
-                                          } else if ( key === currentKey ) {
-                                                newLog.status = 'future';
-                                          }
-                                    }
-                              break;
-                              case 'redo' :
-                                    if ( historyLog.length > ( currentKey + 1 ) ) {
-                                          if ( key === currentKey ) {
-                                                newLog.status = 'previous';
-                                          } else if ( key === ( currentKey + 1 ) ) {
-                                                newLog.status = 'current';
-                                          }
-                                    }
-                              break;
-                        }
-                        newHistoryLog.push( newLog );
-                  });
-                  self.historyLog( newHistoryLog );
             }
       });//$.extend()
 })( wp.customize, jQuery );
