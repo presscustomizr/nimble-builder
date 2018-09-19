37910d63070b1b7860d09bf9c8c3b902732f1262
nikeo <nicolas@presscustomizr.com>
2018-09-19 17:22:38 +0200

    implemented the thumbnail of the preset section

--- inc/sektions/_dev_php/input_tmpl/3_0_2_sek_input_tmpl_sektion_picker.php
+++ inc/sektions/_dev_php/input_tmpl/3_0_2_sek_input_tmpl_sektion_picker.php
@@ -12,19 +12,18 @@ function sek_set_input_tmpl___section_picker( $input_id, $input_data ) {
             $content_collection = array(
                 array(
                   'content-type' => 'preset_section',
-                  'content-id' => 'alternate_text_right',
-                  'title' => 'Image + Text'
-                ),
-                array(
-                  'content-type' => 'preset_section',
-                  'content-id' => 'alternate_text_left',
-                  'title' => 'Text + Image'
+                  'content-id' => 'img_text_one',
+                  'title' => __('2 columns with image and text', 'text-domain' ),
+                  'thumb' => 'img_text_one.jpg'
                 )
             );
             foreach( $content_collection as $_params) {
-                printf('<div draggable="true" data-sek-content-type="%1$s" data-sek-content-id="%2$s"><p>%3$s</p></div>',
+                printf('<div draggable="true" data-sek-content-type="%1$s" data-sek-content-id="%2$s" style="%3$s" title="%4$s"></div>',
                     $_params['content-type'],
                     $_params['content-id'],
+                    sprintf( 'background: url(%1$s) 50% 50% / cover no-repeat;',
+                        NIMBLE_BASE_URL . '/assets/img/section_assets/thumbs/' . $_params['thumb']
+                    ),
                     $_params['title']
                 );
             }
