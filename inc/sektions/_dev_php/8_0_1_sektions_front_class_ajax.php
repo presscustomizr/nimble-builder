04f9850605d13ee2b758f539c5b33f3500b137c6
nikeo <nicolas@presscustomizr.com>
2018-09-19 17:21:26 +0200

    moved wp_ajax_sek_import_attachment code to the SEK_Front_Ajax class

--- inc/sektions/_dev_php/8_0_1_sektions_front_class_ajax.php
+++ inc/sektions/_dev_php/8_0_1_sektions_front_class_ajax.php
@@ -34,6 +34,12 @@ if ( ! class_exists( 'SEK_Front_Ajax' ) ) :
             );
         }
 
+        // Fired in __construct()
+        function _schedule_img_import_ajax_actions() {
+            add_action( 'wp_ajax_sek_import_attachment', array( $this, 'sek_ajax_import_attachemnt' ) );
+        }
+
+
         // hook : 'wp_ajax_sek_get_html_for_injection'
         function sek_get_level_content_for_injection( $params ) {
             // sek_error_log( 'ajax sek_get_level_content_for_injection', $_POST );
@@ -269,7 +275,101 @@ if ( ! class_exists( 'SEK_Front_Ajax' ) ) :
 
 
 
+        /////////////////////////////////////////////////////////////////
+        // hook : wp_ajax_sek_import_attachment
+        function sek_ajax_import_attachemnt() {
+            //sek_error_log( __FUNCTION__ . '$_POST' ,  $_POST);
+            $relative_path = $_POST['rel_path'];
 
+            // Generate the file name from the url.
+            $filename = 'nimble_asset_' . basename( $relative_path );
+            $args = array(
+                'posts_per_page' => 1,
+                'post_type'      => 'attachment',
+                'name'           => trim ( $filename ),
+            );
+
+            // Make sure this img has not already been uploaded
+            $get_attachment = new \WP_Query( $args );
+            //error_log( print_r( $get_attachment->posts, true ) );
+            if ( is_array( $get_attachment->posts ) && array_key_exists(0, $get_attachment->posts) ) {
+                //wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => file already uploaded : ' . $relative_path );
+                $new_attachment = array(
+                    'id'  => $get_attachment->posts[0] -> ID,
+                    'url' => $get_attachment->posts[0] -> guid
+                );
+            }
+
+            // stop now if the attachment was already uploaded
+            if ( isset($new_attachment ) ) {
+                wp_send_json_success( $new_attachment );
+            } else {
+                // Does it exists ?
+                //error_log( "dirname(__FILE__ ) . $relative_path => " . dirname(__FILE__ ) . $relative_path );
+                //error_log("file_exists( dirname(__FILE__ ) . $relative_path => " . file_exists( dirname(__FILE__ ) . $relative_path ) );
+                if ( ! file_exists( NIMBLE_BASE_PATH . $relative_path ) ) {
+                    wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => no file found for relative path : ' . dirname( __FILE__ ) . $relative_path );
+                    return;
+                }
+
+                // Does it return a 200 code ?
+                $url = NIMBLE_BASE_URL . $relative_path;
+                //error_log('$url' .$url );
+                $url_content = wp_safe_remote_get( $url );
+
+                //sek_error_log( __FUNCTION__ . ' response code ?', $url_content['response']['code'] );
+
+                if ( '404' == $url_content['response']['code'] ) {
+                    wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => 404 response when wp_safe_remote_get() url : ' . $url );
+                    return;
+                }
+                $file_content = wp_remote_retrieve_body( $url_content );
+                //sek_error_log( __FUNCTION__ . ' file content ?', $file_content );
+
+                // Is it something ?
+                if ( empty( $file_content ) ) {
+                    wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => empty file_content when wp_remote_retrieve_body() for url : ' . $url );
+                    return;
+                }
+
+                $upload = wp_upload_bits(
+                  $filename,
+                  '',
+                  $file_content
+                );
+
+                $attachment = [
+                  'post_title' => $filename,
+                  'guid' => $upload['url'],
+                ];
+
+                // Set the mime type
+                $info = wp_check_filetype( $upload['file'] );
+                if ( $info ) {
+                    $attachment['post_mime_type'] = $info['type'];
+                } else {
+                    wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => no info available with wp_check_filetype() when setting the mime type of img : ' . $url );
+                    return;
+                }
+
+                $attachment_id = wp_insert_attachment( $attachment, $upload['file'] );
+                // Did everything go well when attempting to insert ?
+                if ( is_wp_error( $attachment_id ) ) {
+                    wp_send_json_error( __CLASS__ . '::' . __FUNCTION__ . ' => problem when trying to wp_insert_attachment() for img : ' . $url );
+                }
+
+                wp_update_attachment_metadata(
+                    $attachment_id,
+                    wp_generate_attachment_metadata( $attachment_id, $upload['file'] )
+                );
+
+                $new_attachment = [
+                  'id' => $attachment_id,
+                  'url' => $upload['url'],
+                ];
+                wp_send_json_success( $new_attachment );
+            }
+        }
 
 
 
