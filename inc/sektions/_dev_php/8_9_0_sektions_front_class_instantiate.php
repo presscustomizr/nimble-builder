ec9880e7a8cb636b80ec14b899f0fc0052718941
nikeo <nicolas@presscustomizr.com>
2018-09-28 10:28:17 +0200

    fixed : skp_is_customizing() shall be called after skope fmk has been loaded

--- inc/sektions/_dev_php/8_9_0_sektions_front_class_instantiate.php
+++ inc/sektions/_dev_php/8_9_0_sektions_front_class_instantiate.php
@@ -1,13 +1,15 @@
 <?php
 // invoked ( and instantiated ) when skp_is_customizing()
 function SEK_CZR_Dyn_Register( $params = array() ) {
-    return SEK_CZR_Dyn_Register::get_instance( $params );
+    if (  skp_is_customizing() ) {
+        return SEK_CZR_Dyn_Register::get_instance( $params );
+    }
 }
+add_action('after_setup_theme', '\Nimble\SEK_CZR_Dyn_Register', 20 );
+
 function SEK_Front( $params = array() ) {
     return SEK_Front_Render_Css::get_instance( $params );
 }
-if (  skp_is_customizing() ) {
-  SEK_CZR_Dyn_Register();
-}
+
 SEK_Front();
 ?>
