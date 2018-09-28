1c61e58188c1a46ae30b31a7e90b7f891d425223
nikeo <nicolas@presscustomizr.com>
2018-09-28 11:02:46 +0200

    fixed : used correct class name to get instance

--- inc/sektions/_dev_php/8_9_0_sektions_front_class_instantiate.php
+++ inc/sektions/_dev_php/8_9_0_sektions_front_class_instantiate.php
@@ -8,7 +8,7 @@ function SEK_CZR_Dyn_Register( $params = array() ) {
 add_action('after_setup_theme', '\Nimble\SEK_CZR_Dyn_Register', 20 );
 
 function SEK_Front( $params = array() ) {
-    return SEK_Front_Render_Css::get_instance( $params );
+    return Sek_Simple_Form::get_instance( $params );
 }
 
 SEK_Front();
