2ae30888483b189c155d6e819eb58590a3c7330b
nikeo <nicolas@presscustomizr.com>
2018-10-09 11:08:39 +0200

    fix : force https after building the dynamic stylesheet url when is_ssl(). fixes #188

--- inc/sektions/_dev_php/dyn_css_builder_and_google_fonts_printer/5_0_2_class-sek-dyn-css-handler.php
+++ inc/sektions/_dev_php/dyn_css_builder_and_google_fonts_printer/5_0_2_class-sek-dyn-css-handler.php
@@ -339,7 +339,7 @@ class Sek_Dyn_CSS_Handler {
         $this->base_url             = $this->_sek_dyn_css_build_base_url();
 
         $this->uri                  = $this->_sek_dyn_css_build_uri();
-        $this->url                  = $this->_sek_dyn_css_build_url();
+        $this->url                  = $this->_ssl_maybe_fix_url( $this->_sek_dyn_css_build_url() );
 
         $this->file_exists          = $this->_sek_dyn_css_file_exists();
 
@@ -348,6 +348,22 @@ class Sek_Dyn_CSS_Handler {
                 $this->mode = self::MODE_INLINE;
             }
         }
+    }
+
+
+    /**
+    * replace http: URL with https: URL
+    * @fix https://github.com/presscustomizr/nimble-builder/issues/188
+    * @param string $url
+    * @return string
+    */
+    function _ssl_maybe_fix_url($url) {
+      // only fix if source URL starts with http://
+      if ( is_ssl() && is_string($url) && stripos($url, 'http://') === 0 ) {
+        $url = 'https' . substr($url, 4);
+      }
+
+      return $url;
     }
 
 
@@ -598,6 +614,7 @@ class Sek_Dyn_CSS_Handler {
      * @return string The absolute CSS file URL
      */
     private function _sek_dyn_css_build_url() {
+
         $base_url = isset( $this->base_uri ) ? $this->base_url : $this->_sek_dyn_css_build_base_uri();
         return trailingslashit( $this->base_url ) . "{$this->id}.css";
     }
