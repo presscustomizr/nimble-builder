20512aa4a61fd4277f48e7c7af0ec10650c92196
nikeo <nicolas@presscustomizr.com>
2018-10-18 16:08:18 +0200

    added the smart load filter when rendering the modue content

--- tmpl/modules/tinymce_editor_module_tmpl.php
+++ tmpl/modules/tinymce_editor_module_tmpl.php
@@ -26,7 +26,7 @@ if ( ! function_exists( '\Nimble\sek_print_tiny_mce_text_content') ) {
             if ( skp_is_customizing() ) {
                 printf('<div title="%3$s" data-sek-input-type="tiny_mce_editor" data-sek-input-id="%1$s">%2$s</div>', $input_id, $content, __( 'Click to edit', 'textdomain_to_be_replaced' ) );
             } else {
-                echo $content;
+                echo apply_filters( 'nimble_parse_for_smart_load', $content );
             }
         }
     }
