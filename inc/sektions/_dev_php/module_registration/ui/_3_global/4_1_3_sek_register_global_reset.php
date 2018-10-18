2f53e8d82d9283b8dbd2eb93bb57bc8b696cf289
nikeo <nicolas@presscustomizr.com>
2018-10-18 16:08:26 +0200

    fixed typo

--- inc/sektions/_dev_php/module_registration/ui/_3_global/4_1_3_sek_register_global_reset.php
+++ inc/sektions/_dev_php/module_registration/ui/_3_global/4_1_3_sek_register_global_reset.php
@@ -4,7 +4,7 @@ function sek_get_module_params_for_sek_global_reset() {
     return array(
         'dynamic_registration' => true,
         'module_type' => 'sek_global_reset',
-        'name' => __('Rest all sections site wide', 'text_domain_to_be_replaced'),
+        'name' => __('Reset all sections site wide', 'text_domain_to_be_replaced'),
         // 'starting_value' => array(
 
         // ),
