1eb64cee50a4020946ba9742dd512b1d62521c67
Rocco Aliberti <raliberti84@gmail.com>
2018-10-31 15:22:25 +0100

    Fix: make sure we enqueue fa assets, needed by the caret

--- inc/sektions/_front_dev_php/module_registration/front/4_1_8_sek_register_menu.php
+++ inc/sektions/_front_dev_php/module_registration/front/4_1_8_sek_register_menu.php
@@ -58,7 +58,15 @@ function sek_get_module_params_for_czr_menu_module() {
             // )
         ),
         'css_selectors' => array( '.sek-menu-module .menu-item' ),//<=@see tmpl/modules/menu_module_tmpl.php
-        'render_tmpl_path' => NIMBLE_BASE_PATH . "/tmpl/modules/menu_module_tmpl.php"
+        'render_tmpl_path' => NIMBLE_BASE_PATH . "/tmpl/modules/menu_module_tmpl.php",
+        'front_assets' => array(
+              'czr-font-awesome' => array(
+                  'type' => 'css',
+                  //'handle' => 'czr-font-awesome',
+                  'src' => NIMBLE_BASE_URL . '/assets/front/fonts/css/fontawesome-all.min.css'
+                  //'deps' => array()
+              )
+        )
     );
 }
  /* ------------------------------------------------------------------------- *
