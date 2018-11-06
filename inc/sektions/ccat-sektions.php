8cd335339f87f7cefb81e95e2215d57a532e88a9
nikeo <nicolas@presscustomizr.com>
2018-11-06 15:27:17 +0100

    intermediate build

--- inc/sektions/ccat-sektions.php
+++ inc/sektions/ccat-sektions.php
@@ -7842,7 +7842,6 @@ if ( ! class_exists( 'SEK_Front_Assets' ) ) :
                 'sekFrontLocalized',
                 array(
                     'isDevMode' => sek_is_dev_mode(),
-                    'ajaxUrl' => admin_url( 'admin-ajax.php' ),
                     'frontNonce' => array( 'id' => 'SEKFrontNonce', 'handle' => wp_create_nonce( 'sek-front-nonce' ) ),
                     'localSeks' => sek_is_debug_mode() ? wp_json_encode( sek_get_skoped_seks() ) : ''
                 )
