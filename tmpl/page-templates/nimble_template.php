23eddcf8f4cb40873cbc2b0316e00524da685ffd
nikeo <nicolas@presscustomizr.com>
2018-11-14 19:51:08 +0100

    implement render_nimble_locations()

--- tmpl/page-templates/nimble_template.php
+++ tmpl/page-templates/nimble_template.php
@@ -7,7 +7,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 
 do_action('nimble_before_content_sections');
-  render_content_sections_for_nimble_template();
+  render_nimble_locations(
+      array( 'loop_start', 'before_content', 'after_content', 'loop_end'),
+      array(
+          // the location rendered even if empty.
+          // This way, the user starts customizing with only one location for the content instead of four
+          // But if the other locations were already customized, they will be printed.
+          'fallback_location' => 'loop_start'
+      )
+  );
 do_action('nimble_after_content_sections');
 
 get_footer();
