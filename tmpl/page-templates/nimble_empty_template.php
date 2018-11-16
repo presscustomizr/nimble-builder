3e86c3228e8b701a4dd5e21d3686295b457d82c7
nikeo <nicolas@presscustomizr.com>
2018-11-16 10:43:38 +0100

    added basic css rules to the nimble empty template

--- tmpl/page-templates/nimble_empty_template.php
+++ tmpl/page-templates/nimble_empty_template.php
@@ -6,41 +6,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="profile" href="https://gmpg.org/xfn/11" />
-  <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-  <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'text_domain_to_replace' ); ?></a>
-  <header id="nimble-header" class="">
-    <?php render_nimble_locations( 'nimble_global_header' ); ?>
-  </header><!-- #masthead -->
-
-  <div id="nimble-content" class="">
-    <?php
-      render_nimble_locations(
-          array( 'loop_start', 'before_content', 'after_content', 'loop_end'),
-          array(
-              // the location rendered even if empty.
-              // This way, the user starts customizing with only one location for the content instead of four
-              // But if the other locations were already customized, they will be printed.
-              'fallback_location' => 'loop_start'
-          )
-      );
-    ?>
-  </div><!-- #content -->
-
-  <footer id="nimble-footer" class="">
-    <?php render_nimble_locations('nimble_global_footer'); ?>
-  </footer><!-- #colophon -->
-
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
+    <div id="nimble-page" class="">
+      <a class="sek-skip-link sek-screen-reader-text" href="#nimble-page"><?php _e( 'Skip to content', 'text_domain_to_replace' ); ?></a>
+      <header id="nimble-header" class="">
+        <?php render_nimble_locations( 'nimble_global_header' ); ?>
+      </header><!-- #masthead -->
+      <div id="nimble-content" class="">
+        <?php
+          render_nimble_locations(
+              array( 'loop_start', 'before_content', 'after_content', 'loop_end'),
+              array(
+                  // the location rendered even if empty.
+                  // This way, the user starts customizing with only one location for the content instead of four
+                  // But if the other locations were already customized, they will be printed.
+                  'fallback_location' => 'loop_start'
+              )
+          );
+        ?>
+      </div><!-- #content -->
+      <footer id="nimble-footer" class="">
+        <?php render_nimble_locations('nimble_global_footer'); ?>
+      </footer><!-- #colophon -->
+    </div><!-- #nimble-page -->
+  <?php wp_footer(); ?>
 </body>
 </html>
