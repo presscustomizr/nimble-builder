348a0b88a26ce90a45939d863b36391cb1e5e65c
nikeo <nicolas@presscustomizr.com>
2018-11-25 20:22:45 +0100

    v1.3.0 build v3

--- inc/sektions/ccat-sektions.php
+++ inc/sektions/ccat-sektions.php
@@ -754,7 +754,12 @@ function sek_filter_skp_get_skope_id( $skope_id, $level ) {
     }
     return $skope_id;
 }
-
+function render_content_sections_for_nimble_template() {
+    Nimble_Manager()->render_nimble_locations(
+        array_keys( Nimble_Manager()->default_locations ),//array( 'loop_start', 'before_content', 'after_content', 'loop_end'),
+        array( 'fallback_location' => 'loop_start' )
+    );
+}
 ?><?php
 function sek_maybe_do_version_mapping() {
     if ( ! is_user_logged_in() || ! current_user_can( 'edit_theme_options' ) )
@@ -8702,14 +8707,6 @@ if ( ! class_exists( 'SEK_Front_Render' ) ) :
 
             }//render_nimble_locations()
         }
-        function render_content_sections_for_nimble_template() {
-            render_nimble_locations(
-                array_keys( \Nimble\Nimble_Manager()->default_locations ),//array( 'loop_start', 'before_content', 'after_content', 'loop_end'),
-                array( 'fallback_location' => 'loop_start' )
-            );
-        }
-
-
 
 
 
