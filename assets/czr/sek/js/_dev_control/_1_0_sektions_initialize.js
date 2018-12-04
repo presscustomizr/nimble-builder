04f21b5e859512dea69a4cfdf0e56cc800854a5d
nikeo <nicolas@presscustomizr.com>
2018-12-04 15:09:41 +0100

    SETTING_UPDATE_BUFFER reduced to 100 ms. following previous fixes for #244

--- assets/czr/sek/js/_dev_control/_1_0_sektions_initialize.js
+++ assets/czr/sek/js/_dev_control/_1_0_sektions_initialize.js
@@ -17,7 +17,7 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
 
                   // _.debounce param when updating the UI setting
                   // prevent hammering server + fixes https://github.com/presscustomizr/nimble-builder/issues/244
-                  self.SETTING_UPDATE_BUFFER = 200;
+                  self.SETTING_UPDATE_BUFFER = 100;
 
                   // Define a default value for the sektion setting value, used when no server value has been sent
                   // @see php function
