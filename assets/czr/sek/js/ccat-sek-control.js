b9f83c88a854a04aa41e80b78ad4650f2f9f6e32
nikeo <nicolas@presscustomizr.com>
2018-12-15 18:26:50 +0100

    v1.4.0 build v3

--- assets/czr/sek/js/ccat-sek-control.js
+++ assets/czr/sek/js/ccat-sek-control.js
@@ -8241,6 +8241,7 @@ var CZRSeksPrototype = CZRSeksPrototype || {};
                   name : api.czr_sektions.getRegisteredModuleProperty( 'czr_menu_content_child', 'name' ),
                   has_mod_opt : false,
                   ready_on_section_expanded : true,
+                  ready_on_control_event : 'sek-accordion-expanded',// triggered in ::scheduleModuleAccordion()
                   defaultItemModel : api.czr_sektions.getDefaultItemModelFromRegisteredModuleData( 'czr_menu_content_child' )
             }
       });
