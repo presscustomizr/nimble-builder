0cc60fb7cf3a84179c5bf82d46d4ac530d8a9b19
nikeo <nicolas@presscustomizr.com>
2018-12-21 22:17:19 +0100

    fixed : input id

--- inc/sektions/_front_dev_php/module_registration/ui/_3_global/4_1_6_sek_register_beta_features.php
+++ inc/sektions/_front_dev_php/module_registration/ui/_3_global/4_1_6_sek_register_beta_features.php
@@ -12,13 +12,14 @@ function sek_get_module_params_for_sek_global_beta_features() {
         // 'validate_callback' => 'function_prefix_to_be_replaced_validate_callback__czr_social_module',
         'tmpl' => array(
             'item-inputs' => array(
-                'header-footer' => array(
+                'beta-enabled' => array(
                     'input_type'  => 'gutencheck',
                     'title'       => __('Enable beta features', 'text_domain_to_be_replaced'),
                     'default'     => 0,
                     'title_width' => 'width-80',
                     'input_width' => 'width-20',
-                    'notice_after' => __( 'Check this option to enable the upcoming features of Nimble, currently in beta version. Available beta features as of December 2018 : header and footer customization, menu module, widget area module', 'text_domain_to_be_replaced')
+                    'notice_before_title' => __( 'Check this option to try the upcoming features of the Nimble Builder. Available beta features as of December 2018 : header and footer customization, menu module, widget area module.', 'text_domain_to_be_replaced'),
+                    'notice_after' => __( 'Be sure to refresh the customizer before you start using the beta features.', 'text_domain_to_be_replaced')
                 ),
             )
         )//tmpl
