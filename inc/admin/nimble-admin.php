340019727c23ed5af706305300a8604d0ba10fdd
nikeo <nicolas@presscustomizr.com>
2018-12-21 16:49:25 +0100

    improved : re-phrased the welcome message

--- inc/admin/nimble-admin.php
+++ inc/admin/nimble-admin.php
@@ -466,7 +466,8 @@ function sek_render_welcome_notice() {
       </div>
       <h1><?php echo apply_filters( 'nimble_update_notice', __('Welcome to the Nimble Builder for WordPress :D', 'nimble' ) ); ?></h1>
       <h3><?php _e( 'The Nimble Builder takes the native WordPress customizer to a level you\'ve never seen before.', 'nimble' ); ?></h3>
-      <h3><?php _e( 'Nimble allows you to insert content into any page and edit it in <i>real</i> live preview. The plugin automatically creates fluid layouts for your visitors using smartphones or tablets, without the need to add complex code. Nimble Builder comes with a set of carefully crafted and attractive pre-built sections.', 'nimble' ); ?></h3>
+      <h3><?php _e( 'Nimble allows you to drag and drop content modules, or pre-built section templates, into <u>any context</u> of your site, including search results or 404 pages. You can edit your pages in <i>real time</i> from the live customizer, and then publish when you are happy of the result, or save for later.', 'nimble' ); ?></h3>
+      <h3><?php _e( 'The plugin automatically creates fluid and responsive sections for a pixel-perfect rendering on smartphones and tablets, without the need to add complex code.', 'nimble' ); ?></h3>
       <?php printf( '<a href="%1$s" target="_blank" class="button button-primary button-hero"><span class="dashicons dashicons-admin-appearance"></span> %2$s</a>',
           esc_url( add_query_arg(
               array(
