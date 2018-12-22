19c4406bb027804e40f020a5b4744233832db0e6
nikeo <nicolas@presscustomizr.com>
2018-12-22 18:48:18 +0100

    fixed campaign link missed a slash

--- inc/sektions/_customizer_dev_php/1_0_0_sektions_customizer_assets.php
+++ inc/sektions/_customizer_dev_php/1_0_0_sektions_customizer_assets.php
@@ -590,7 +590,7 @@ function sek_print_nimble_customizer_tmpl() {
               <span class="screen-reader-text"><?php _e('Global settings', 'text_domain'); ?></span>
             </button>
           </div>
-          <div class="sek-nimble-doc" data-doc-href="https://docs.presscustomizr.com/collection/334-nimble-builder?utm_source=usersite&utm_medium=link&utm_campaign=nimble-customizer-topbar">
+          <div class="sek-nimble-doc" data-doc-href="https://docs.presscustomizr.com/collection/334-nimble-builder/?utm_source=usersite&utm_medium=link&utm_campaign=nimble-customizer-topbar">
             <div class="sek-nimble-icon"><img src="<?php echo NIMBLE_BASE_URL.'/assets/img/nimble/nimble_icon.svg?ver='.NIMBLE_VERSION; ?>" alt="<?php _e('Nimble Builder','text_domain_to_replace'); ?>" title="<?php _e('Nimble online documentation', 'text_domain'); ?>"/></div>
             <span class="sek-pointer" title="<?php _e('Nimble online documentation', 'text_domain'); ?>"><?php _e('Nimble online documentation', 'text_domain'); ?></span>
             <button class="far fa-question-circle" type="button" title="<?php _e('Nimble online documentation', 'text_domain'); ?>" data-nimble-state="enabled">
