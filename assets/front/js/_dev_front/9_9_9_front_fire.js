198d716c1482d1f66ff078175d1a2127e776fe67
nikeo <nicolas@presscustomizr.com>
2019-03-22 11:12:50 +0100

    fixed : animated scroll to anchor link doesn't work on links like https://mysite/#services. fixes #413

--- assets/front/js/_dev_front/9_9_9_front_fire.js
+++ assets/front/js/_dev_front/9_9_9_front_fire.js
@@ -94,20 +94,51 @@ jQuery( function($){
     //     });
     // }
 
-    // animate menu item to Nimble anchors
-    $('body').find('.sek-menu-module, .menu, .nav' ).on( 'click', '.menu-item [href^="#"]', function( evt ){
-          evt.preventDefault();
-          var anchorCandidate = $(this).attr('href');
-          anchorCandidate = 'string' === typeof( anchorCandidate ) ? anchorCandidate.replace('#','') : '';
+    // does the same as new URL(url)
+    // but support IE.
+    // @see https://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
+    // @see https://gist.github.com/acdcjunior/9820040
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/URL#Properties
+    var parseURL = function(url) {
+          var parser = document.createElement("a");
+          parser.href = url;
+          // IE 8 and 9 dont load the attributes "protocol" and "host" in case the source URL
+          // is just a pathname, that is, "/example" and not "http://domain.com/example".
+          parser.href = parser.href;
 
-          if ( '' !== anchorCandidate || null !== anchorCandidate ) {
-                var $anchorCandidate = $('[data-sek-level="location"]' ).find( '[id="' + anchorCandidate + '"]');
-                if ( 1 === $anchorCandidate.length ) {
-                      $('html, body').animate({
-                            scrollTop : $anchorCandidate.offset().top - 150
-                      }, 'slow');
-                }
+          // copies all the properties to this object
+          var properties = ['host', 'hostname', 'hash', 'href', 'port', 'protocol', 'search'];
+          for (var i = 0, n = properties.length; i < n; i++) {
+            this[properties[i]] = parser[properties[i]];
           }
+
+          // pathname is special because IE takes the "/" of the starting of pathname
+          this.pathname = (parser.pathname.charAt(0) !== "/" ? "/" : "") + parser.pathname;
+    };
+
+    var $root = $('html, body');
+    // animate menu item to Nimble anchors
+    $('body').find('.menu-item' ).on( 'click', 'a', function( evt ){
+          // problem to solve : users want to define anchor links that work inside a page, but also from other pages.
+          // @see https://github.com/presscustomizr/nimble-builder/issues/413
+          var menuItemUrl = $(this).attr('href');
+          if ( '' === menuItemUrl || null === menuItemUrl || 'string' !== typeof( menuItemUrl ) || -1 === menuItemUrl.indexOf('#') )
+            return;
+
+          // an anchor link looks like this : http://mysite.com/contact/#anchor
+          var itemURLObject = new parseURL( menuItemUrl ),
+              _currentPageUrl = new parseURL( window.document.location.href );
+
+          if( itemURLObject.pathname !== _currentPageUrl.pathname )
+            return;
+          if( 'string' !== typeof(itemURLObject.hash) || '' === itemURLObject.hash )
+            return;
+          var $nimbleTargetCandidate = $('[data-sek-level="location"]' ).find( '[id="' + itemURLObject.hash.replace('#','') + '"]');
+          if ( 1 !== $nimbleTargetCandidate.length )
+            return;
+
+          evt.preventDefault();
+          $root.animate({ scrollTop : $nimbleTargetCandidate.offset().top - 150 }, 400 );
     });
 });
 
