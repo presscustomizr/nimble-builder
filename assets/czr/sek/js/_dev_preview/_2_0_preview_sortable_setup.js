ba7fd2050d8831dd5755b03510596b36d3308572
nikeo <nicolas@presscustomizr.com>
2019-06-11 22:54:06 +0200

    fixed : resizable not re-instantiated when moving columns

--- assets/czr/sek/js/_dev_preview/_2_0_preview_sortable_setup.js
+++ assets/czr/sek/js/_dev_preview/_2_0_preview_sortable_setup.js
@@ -380,6 +380,14 @@ var SekPreviewPrototype = SekPreviewPrototype || {};
                                     from_sektion : from_sektion,
                                     to_sektion : to_sektion
                               });
+
+                              // inform the parent section that it's been refreshed
+                              //=> will be listened to by columns to re-instantiate sortable, resizable
+                              $('div[data-sek-id="' + from_sektion + '"]' ).trigger('sek-level-refreshed');
+                              if ( from_sektion !== to_sektion ) {
+                                  $('div[data-sek-id="' + to_sektion + '"]' ).trigger('sek-level-refreshed');
+                              }
+
                         }
                   }));
             },//self.makeColumnsSortableInSektion
