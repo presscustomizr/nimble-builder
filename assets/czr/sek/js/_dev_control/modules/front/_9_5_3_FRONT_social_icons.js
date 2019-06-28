af032566b905174e1de6c7f7253952e4753de25b
nikeo <nicolas@presscustomizr.com>
2019-06-28 10:13:33 +0200

    implemented a title update on change, to display the icon

--- assets/czr/sek/js/_dev_control/modules/front/_9_5_3_FRONT_social_icons.js
+++ assets/czr/sek/js/_dev_control/modules/front/_9_5_3_FRONT_social_icons.js
@@ -7,7 +7,20 @@
                   var module = this;
 
                   // //EXTEND THE DEFAULT CONSTRUCTORS FOR MONOMODEL
-                  //module.itemConstructor = api.CZRItem.extend( module.CZRItemConstructor || {} );
+                  module.itemConstructor = api.CZRItem.extend( module.CZRItemConstructor || {} );
+
+                  // module.isReady.then( function() {
+                  //       if ( _.isUndefined( module.preItem ) )
+                  //         return;
+                  //       //specific update for the item preModel on social-icon change
+                  //       module.preItem.bind( function( to, from ) {
+                  //             if ( ! _.has(to, 'icon') )
+                  //               return;
+                  //             if ( _.isEqual( to['icon'], from['icon'] ) )
+                  //               return;
+                  //             module.updateItemModel( module.preItem, true );
+                  //       });
+                  // });
 
                   // run the parent initialize
                   // Note : must be always invoked always after the input / item class extension
@@ -15,76 +28,86 @@
                   api.CZRDynModule.prototype.initialize.call( module, id, options );
             },//initialize
 
+
+            // overrides the default fmk method which generates a too long id for each item, like : "czr_social_icons_settings_child_2"
+            // this method generates a uniq GUID id for each item
+            generateItemId : function() {
+                    return api.czr_sektions.guid();
+            },
+
+
             //////////////////////////////////////////////////////////
             /// ITEM CONSTRUCTOR
             //////////////////////////////////////////
+            CZRItemConstructor : {
+                  //overrides the parent ready
+                  ready : function() {
+                        var item = this;
+                        //wait for the input collection to be populated,
+                        //and then set the input visibility dependencies
+                        item.inputCollection.bind( function( col ) {
+                              if( _.isEmpty( col ) )
+                                return;
+                              try { item.setInputVisibilityDeps(); } catch( er ) {
+                                    api.errorLog( 'item.setInputVisibilityDeps() : ' + er );
+                              }
+                        });//item.inputCollection.bind()
 
-            // CZRItemConstructor : {
-            //       //overrides the parent ready
-            //       ready : function() {
-            //             var item = this;
-            //             //wait for the input collection to be populated,
-            //             //and then set the input visibility dependencies
-            //             item.inputCollection.bind( function( col ) {
-            //                   if( _.isEmpty( col ) )
-            //                     return;
-            //                   try { item.setInputVisibilityDeps(); } catch( er ) {
-            //                         api.errorLog( 'item.setInputVisibilityDeps() : ' + er );
-            //                   }
-            //             });//item.inputCollection.bind()
+                        // //update the item model on social-icon change
+                        // item.bind('icon:changed', function(){
+                        //       console.log('MERDE ?');
+                        //       //item.module.updateItemModel( item );
+                        // });
+                        //fire the parent
+                        api.CZRItem.prototype.ready.call( item );
+                  },
 
-            //             //fire the parent
-            //             api.CZRItem.prototype.ready.call( item );
-            //       },
-            //       //Fired when the input collection is populated
-            //       //At this point, the inputs are all ready (input.isReady.state() === 'resolved') and we can use their visible Value ( set to true by default )
-            //       setInputVisibilityDeps : function() {
-            //             var item = this,
-            //                 module = item.module;
+                  //
+                  _buildTitle : function( title, icon, color ) {
+                          var item = this,
+                              module     = item.module;
+                          title = title || ( 'string' === typeof(icon) ? api.CZR_Helpers.capitalize( icon.replace( 'fa-', '') ) : '' );
+                          title = api.CZR_Helpers.truncate(title, 20);
+                          color = color || module.defaultSocialColor;
 
-            //             //Internal item dependencies
-            //             item.czr_Input.each( function( input ) {
-            //                   switch( input.id ) {
-            //                         case 'img-type' :
-            //                               _.each( [ 'img-id', 'img-size' ] , function( _inputId_ ) {
-            //                                     try { api.czr_sektions.scheduleVisibilityOfInputId.call( input, _inputId_, function() {
-            //                                           var bool = false;
-            //                                           switch( _inputId_ ) {
-            //                                                 case 'img-id' :
-            //                                                       bool = 'custom' === input();
-            //                                                 break;
-            //                                                 default :
-            //                                                       bool = 'none' !== input();
-            //                                                 break;
-            //                                           }
-            //                                           return bool;
-            //                                     }); } catch( er ) {
-            //                                           api.errare( 'Featured pages module => error in setInputVisibilityDeps', er );
-            //                                     }
-            //                               });
-            //                         break;
-            //                         case 'content-type' :
-            //                               _.each( [ 'content-custom-text' ] , function( _inputId_ ) {
-            //                                     try { api.czr_sektions.scheduleVisibilityOfInputId.call( input, _inputId_, function() {
-            //                                           return 'custom' === input();
-            //                                     }); } catch( er ) {
-            //                                           api.errare( 'Featured pages module => error in setInputVisibilityDeps', er );
-            //                                     }
-            //                               });
-            //                         break;
-            //                         case 'btn-display' :
-            //                               _.each( [ 'btn-custom-text' ] , function( _inputId_ ) {
-            //                                     try { api.czr_sektions.scheduleVisibilityOfInputId.call( input, _inputId_, function() {
-            //                                           return input();
-            //                                     }); } catch( er ) {
-            //                                           api.errare( 'Featured pages module => error in setInputVisibilityDeps', er );
-            //                                     }
-            //                               });
-            //                         break;
-            //                   }
-            //             });
-            //       }
-            // },//CZRItemConstructor
+                          return '<div><span class="' + icon + '" style="color:' + color + '"></span> ' + title + '</div>';
+                  },
+
+                  //overrides the default parent method by a custom one
+                  //at this stage, the model passed in the obj is up to date
+                  writeItemViewTitle : function( model ) {
+                          var item = this,
+                              module     = item.module,
+                              _model = model || item(),
+                              _title = ( _model['icon'] ? _model['icon'] : '' ).replace('fa-', '').replace('envelope', 'email').replace( 'far', '').replace( 'fab', '').replace( 'fas', '');
+
+                          $( '.' + module.control.css_attr.item_title , item.container ).html(
+                            item._buildTitle( _title, _model['icon'], _model['color_css'] )
+                          );
+                  },
+
+                  //Fired when the input collection is populated
+                  //At this point, the inputs are all ready (input.isReady.state() === 'resolved') and we can use their visible Value ( set to true by default )
+                  setInputVisibilityDeps : function() {
+                        var item = this,
+                            module = item.module;
+
+                        //Internal item dependencies
+                        item.czr_Input.each( function( input ) {
+                              switch( input.id ) {
+                                    case 'use_custom_color_on_hover' :
+                                          _.each( [ 'color_hover' ] , function( _inputId_ ) {
+                                                try { api.czr_sektions.scheduleVisibilityOfInputId.call( input, _inputId_, function() {
+                                                      return input();
+                                                }); } catch( er ) {
+                                                      api.errare( 'Featured pages module => error in setInputVisibilityDeps', er );
+                                                }
+                                          });
+                                    break;
+                              }
+                        });
+                  }
+            },//CZRItemConstructor
       };//Constructor
 
       //provides a description of each module
