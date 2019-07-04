1a6f04d50ed5f32516478b11b1622b2b3f6de71d
nikeo <nicolas@presscustomizr.com>
2019-07-04 23:04:02 +0200

    expand the first control of the module

--- assets/czr/sek/js/_dev_control/modules/front/_9_2_1_FRONT_menu.js
+++ assets/czr/sek/js/_dev_control/modules/front/_9_2_1_FRONT_menu.js
@@ -19,7 +19,7 @@
                   crud : false,
                   name : api.czr_sektions.getRegisteredModuleProperty( 'czr_menu_content_child', 'name' ),
                   has_mod_opt : false,
-                  ready_on_section_expanded : true,
+                  ready_on_section_expanded : false,
                   ready_on_control_event : 'sek-accordion-expanded',// triggered in ::scheduleModuleAccordion()
                   defaultItemModel : api.czr_sektions.getDefaultItemModelFromRegisteredModuleData( 'czr_menu_content_child' )
             }
@@ -48,7 +48,7 @@
                   crud : false,
                   name : api.czr_sektions.getRegisteredModuleProperty( 'czr_menu_mobile_options', 'name' ),
                   has_mod_opt : false,
-                  ready_on_section_expanded : true,
+                  ready_on_section_expanded : false,
                   ready_on_control_event : 'sek-accordion-expanded',// triggered in ::scheduleModuleAccordion()
                   defaultItemModel : api.czr_sektions.getDefaultItemModelFromRegisteredModuleData( 'czr_menu_mobile_options' )
             }
