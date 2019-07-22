c1b080a10d0d92d06e9ad3dcec88b5a0fa499774
nikeo <nicolas@presscustomizr.com>
2019-07-22 13:11:04 +0200

    fixed : removed registration for unused js module

--- assets/czr/sek/js/_dev_control/modules/front/_9_5_4_FRONT_img_slider.js
+++ assets/czr/sek/js/_dev_control/modules/front/_9_5_4_FRONT_img_slider.js
@@ -535,34 +535,3 @@
             }
       });
 })( wp.customize , jQuery, _ );
-
-
-
-
-
-
-/* ------------------------------------------------------------------------- *
- *  SLIDER DESIGN OPTIONS
-/* ------------------------------------------------------------------------- */
-( function ( api, $, _ ) {
-      //provides a description of each module
-      //=> will determine :
-      //1) how to initialize the module model. If not crud, then the initial item(s) model shall be provided
-      //2) which js template(s) to use : if crud, the module template shall include the add new and pre-item elements.
-      //   , if crud, the item shall be removable
-      //3) how to render : if multi item, the item content is rendered when user click on edit button.
-      //    If not multi item, the single item content is rendered as soon as the item wrapper is rendered.
-      //4) some DOM behaviour. For example, a multi item shall be sortable.
-      api.czrModuleMap = api.czrModuleMap || {};
-      $.extend( api.czrModuleMap, {
-            czr_img_slider_fonts_child : {
-                  //mthds : Constructor,
-                  crud : false,
-                  name : api.czr_sektions.getRegisteredModuleProperty( 'czr_img_slider_fonts_child', 'name' ),
-                  has_mod_opt : false,
-                  ready_on_section_expanded : false,
-                  ready_on_control_event : 'sek-accordion-expanded',// triggered in ::scheduleModuleAccordion()
-                  defaultItemModel : api.czr_sektions.getDefaultItemModelFromRegisteredModuleData( 'czr_img_slider_fonts_child' )
-            }
-      });
-})( wp.customize , jQuery, _ );
