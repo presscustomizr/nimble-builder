803787e5336035093c85b59488c129e9c5c22d79
nikeo <nicolas@presscustomizr.com>
2019-09-17 17:45:39 +0200

    v1.8.21 build v5

--- inc/sektions/ccat-sektions.php
+++ inc/sektions/ccat-sektions.php
@@ -7416,6 +7416,134 @@ function sek_get_module_params_for_czr_simple_html_module() {
                     'title'       => __( 'HTML Content' , 'text_doma' ),
                     'refresh_markup' => '.sek-module-inner'
                     //'code_type' => 'text/html' //<= use 'text/css' to instantiate the code mirror as CSS editor, which by default will be an HTML editor
+                ),
+                'font_family_css' => array(
+                    'input_type'  => 'font_picker',
+                    'title'       => __('Font family', 'text_doma'),
+                    'default'     => '',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'refresh_fonts' => true,
+                    'css_identifier' => 'font_family',
+                    'html_before' => '<hr/><h3>' . __('TEXT OPTIONS') .'</h3>'
+                ),
+                'font_size_css'       => array(
+                    'input_type'  => 'range_with_unit_picker_device_switcher',
+                    'title'       => __( 'Font size', 'text_doma' ),
+                    // the default value is commented to fix https://github.com/presscustomizr/nimble-builder/issues/313
+                    // => as a consequence, when a module uses the font child module, the default font-size rule must be defined in the module SCSS file.
+                    //'default'     => array( 'desktop' => '16px' ),
+                    'min' => 0,
+                    'max' => 100,
+                    'title_width' => 'width-100',
+                    'width-100'         => true,
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'font_size'
+                ),//16,//"14px",
+                'line_height_css'     => array(
+                    'input_type'  => 'range_with_unit_picker',
+                    'title'       => __( 'Line height', 'text_doma' ),
+                    'default'     => '1.5em',
+                    'min' => 0,
+                    'max' => 10,
+                    'step' => 0.1,
+                    'width-100'         => true,
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'line_height'
+                ),//24,//"20px",
+                'color_css'           => array(
+                    'input_type'  => 'wp_color_alpha',
+                    'title'       => __('Text color', 'text_doma'),
+                    'default'     => '',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'width-100'   => true,
+                    'css_identifier' => 'color'
+                ),//"#000000",
+                'color_hover_css'     => array(
+                    'input_type'  => 'wp_color_alpha',
+                    'title'       => __('Text color on mouse over', 'text_doma'),
+                    'default'     => '',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'width-100'   => true,
+                    'title_width' => 'width-100',
+                    'css_identifier' => 'color_hover'
+                ),//"#000000",
+                'font_weight_css'     => array(
+                    'input_type'  => 'simpleselect',
+                    'title'       => __('Font weight', 'text_doma'),
+                    'default'     => 400,
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'font_weight',
+                    'choices'            => sek_get_select_options_for_input_id( 'font_weight_css' )
+                ),//null,
+                'font_style_css'      => array(
+                    'input_type'  => 'simpleselect',
+                    'title'       => __('Font style', 'text_doma'),
+                    'default'     => 'inherit',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'font_style',
+                    'choices'            => sek_get_select_options_for_input_id( 'font_style_css' )
+                ),//null,
+                'text_decoration_css' => array(
+                    'input_type'  => 'simpleselect',
+                    'title'       => __('Text decoration', 'text_doma'),
+                    'default'     => 'none',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'text_decoration',
+                    'choices'            => sek_get_select_options_for_input_id( 'text_decoration_css' )
+                ),//null,
+                'text_transform_css'  => array(
+                    'input_type'  => 'simpleselect',
+                    'title'       => __('Text transform', 'text_doma'),
+                    'default'     => 'none',
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'text_transform',
+                    'choices'            => sek_get_select_options_for_input_id( 'text_transform_css' )
+                ),//null,
+
+                'letter_spacing_css'  => array(
+                    'input_type'  => 'range_simple',
+                    'title'       => __( 'Letter spacing', 'text_doma' ),
+                    'default'     => 0,
+                    'min'         => 0,
+                    'step'        => 1,
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'css_identifier' => 'letter_spacing',
+                    'width-100'   => true,
+                ),//0,
+                // Note : always use the suffix '_flag_important' to name an input controling the !important css flag @see Nimble\sek_add_css_rules_for_css_sniffed_input_id
+                'fonts___flag_important'  => array(
+                    'input_type'  => 'nimblecheck',
+                    'title'       => __('Apply the style options in priority (uses !important).', 'text_doma'),
+                    'default'     => 0,
+                    'refresh_markup' => false,
+                    'refresh_stylesheet' => true,
+                    'title_width' => 'width-80',
+                    'input_width' => 'width-20',
+                    // declare the list of input_id that will be flagged with !important when the option is checked
+                    // @see sek_add_css_rules_for_css_sniffed_input_id
+                    // @see Nsek_is_flagged_important
+                    'important_input_list' => array(
+                        'font_family_css',
+                        'font_size_css',
+                        'line_height_css',
+                        'font_weight_css',
+                        'font_style_css',
+                        'text_decoration_css',
+                        'text_transform_css',
+                        'letter_spacing_css',
+                        'color_css',
+                        'color_hover_css'
+                    )
                 )
             )
         ),
