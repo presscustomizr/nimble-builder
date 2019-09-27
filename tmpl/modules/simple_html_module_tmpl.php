57c3b3fbb40f337217099eba521f6021c227d9b1
nikeo <nicolas@presscustomizr.com>
2019-09-27 17:16:22 +0200

    HTML module => parse template tags

--- tmpl/modules/simple_html_module_tmpl.php
+++ tmpl/modules/simple_html_module_tmpl.php
@@ -15,6 +15,9 @@ if ( ! function_exists( 'Nimble\sek_print_html_content') ) {
         if ( empty( $html_content ) ) {
             echo Nimble_Manager()->sek_get_input_placeholder_content( 'text', 'html_content' );
         } else {
+            // added September 2019 when revamping presscustomizr.com
+            $html_content = sek_parse_template_tags( $html_content );
+
             //TODO: move add_filter 'sek_html_content' somewhere else so it's called once
             //and we won't need to remove it
             //Also consider to add several other filter callbacks e.g. wptexturize, wpemoji... : see default-filters for 'the_content' in wp-incudes/default-filters.php
