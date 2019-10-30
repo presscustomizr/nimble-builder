08fd13a3a0b8bafb367ca00cf22de557fdc2fb8e
nikeo <nicolas@presscustomizr.com>
2019-10-30 15:32:39 +0100

    added : page template => added a way to add page templates from a child theme. fixes #400

--- inc/sektions/_front_dev_php/0_5_0_sektions_functions_definitions.php
+++ inc/sektions/_front_dev_php/0_5_0_sektions_functions_definitions.php
@@ -820,7 +820,67 @@ function sek_booleanize_checkbox_val( $val ) {
 }
 
 
+/* ------------------------------------------------------------------------- *
+ *   TEMPLATE OVERRIDE HELPERS
+/* ------------------------------------------------------------------------- */
+// TEMPLATES PATH
+// added for #532, october 2019
+/**
+ * Returns the path to the NIMBLE templates directory
+ * inspîred from /wp-content/plugins/easy-digital-downloads/includes/template-functions.php
+ */
+function sek_get_templates_dir() {
+  return NIMBLE_BASE_PATH . "/tmpl";
+}
 
+// added for #532, october 2019
+/* Returns the template directory name.
+ * inspîred from /wp-content/plugins/easy-digital-downloads/includes/template-functions.php
+*/
+function sek_get_theme_template_dir_name() {
+  return trailingslashit( apply_filters( 'nimble_templates_dir', 'nimble_templates' ) );
+}
+
+
+// added for #532, october 2019
+/**
+ * Returns a list of paths to check for template locations
+ * inspîred from /wp-content/plugins/easy-digital-downloads/includes/template-functions.php
+ */
+function sek_get_theme_template_base_paths() {
+
+  $template_dir = sek_get_theme_template_dir_name();
+
+  $file_paths = array(
+    1 => trailingslashit( get_stylesheet_directory() ) . $template_dir,
+    10 => trailingslashit( get_template_directory() ) . $template_dir
+  );
+
+  $file_paths = apply_filters( 'nimble_template_paths', $file_paths );
+
+  // sort the file paths based on priority
+  ksort( $file_paths, SORT_NUMERIC );
+
+  return array_map( 'trailingslashit', $file_paths );
+}
+
+
+// @return path string
+// added for #400
+function sek_maybe_get_overriden_local_template_path( $template_name = '') {
+    if ( empty( $template_name ) )
+      return;
+    $overriden_template_path = '';
+    // try locating this template file by looping through the template paths
+    // inspîred from /wp-content/plugins/easy-digital-downloads/includes/template-functions.php
+    foreach( sek_get_theme_template_base_paths() as $path_candidate ) {
+      if( file_exists( $path_candidate . 'page-templates/' . $template_name ) ) {
+        $overriden_template_path = $path_candidate . 'page-templates/' . $template_name;
+        break;
+      }
+    }
+    return $overriden_template_path;
+}
 
 
 /* ------------------------------------------------------------------------- *
@@ -828,20 +888,31 @@ function sek_booleanize_checkbox_val( $val ) {
 /* ------------------------------------------------------------------------- */
 // @return mixed null || string
 function sek_get_locale_template(){
-    $path = null;
+    $template_path = null;
     $local_template_data = sek_get_local_option_value( 'template' );
     if ( ! empty( $local_template_data ) && ! empty( $local_template_data['local_template'] ) && 'default' !== $local_template_data['local_template'] ) {
         $template_file_name = $local_template_data['local_template'];
-        $path = apply_filters( 'nimble_get_locale_template_path', NIMBLE_BASE_PATH . '/tmpl/page-templates/' . $template_file_name . '.php', $template_file_name );
-        if ( file_exists( $path ) ) {
-            $template = $path;
+        $template_file_name = $template_file_name . '.php';
+
+        // Default page tmpl path looks like : NIMBLE_BASE_PATH . "/tmpl/page-template/nimble_template.php",
+        $overriden_template_path = sek_maybe_get_overriden_local_template_path( $template_file_name );
+        if ( !empty( $overriden_template_path ) ) {
+            $template_path = $overriden_template_path;
         } else {
-            sek_error_log( __FUNCTION__ .' the custom template does not exist', $path );
-            $path = null;
+            $template_path = sek_get_templates_dir() . "/page-templates/{$template_file_name}";
+        }
+
+        $template_path = apply_filters( 'nimble_get_locale_template_path', $template_path, $template_file_name );
+
+        if ( ! file_exists( $template_path ) ) {
+            sek_error_log( __FUNCTION__ .' the custom template does not exist', $template_path );
+            $template_path = null;
         }
     }
-    return $path;
+    return $template_path;
 }
+
+
 
 
 // @param $option_name = string
