0cba96c571a11641d7f37f3bf12a1b1fdffddf2c
nikeo <nicolas@presscustomizr.com>
2019-11-06 23:07:40 +0100

    following https://github.com/presscustomizr/nimble-builder/commit/84a0b3c59ed0873ac6251fa18c0a10d365b7ab35, make sure the override "wins" even when template path is filtered

--- inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
+++ inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
@@ -567,17 +567,20 @@ if ( ! class_exists( 'SEK_Front_Render' ) ) :
                     $template_name = sek_get_registered_module_type_property( $module_type, 'render_tmpl_path' );
                     $template_name = ltrim( $template_name, '/' );
 
-                    $template_path = '';
+                    // first define the default template path
+                    $template_path = sek_get_templates_dir() . "/modules/{$template_name}";
+                    // make this filtrable
+                    $render_tmpl_path = apply_filters( 'nimble_module_tmpl_path', $template_path, $module_type );
+
+                    // Then check if there's an override
                     $overriden_template_path = $this->sek_maybe_get_overriden_template_path_for_module( $template_name );
+
                     $is_module_template_overriden = false;
                     if ( !empty( $overriden_template_path ) ) {
-                        $template_path = $overriden_template_path;
+                        $render_tmpl_path = $overriden_template_path;
                         $is_module_template_overriden = true;
-                    } else {
-                        $template_path = sek_get_templates_dir() . "/modules/{$template_name}";
                     }
 
-                    $render_tmpl_path = apply_filters( 'nimble_module_tmpl_path', $template_path, $module_type );
 
                     printf('<div data-sek-level="module" data-sek-id="%1$s" data-sek-module-type="%2$s" class="sek-module %3$s %4$s" %5$s %6$s %7$s %8$s %9$s>',
                         $id,
