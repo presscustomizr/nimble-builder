b0c298673363221c01e31adb40ec4159e484a122
nikeo <nicolas@presscustomizr.com>
2020-01-04 18:18:35 +0100

    fixed : [Shortcode module] Allow user to switch between flex and block display. fixes #575

--- tmpl/modules/shortcode_module_tmpl.php
+++ tmpl/modules/shortcode_module_tmpl.php
@@ -5,20 +5,25 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+$model = Nimble_Manager()->model;
+$module_type = $model['module_type'];
+$value = array_key_exists( 'value', $model ) ? $model['value'] : array();
+
 if ( ! function_exists( 'Nimble\sek_print_shortcode_module_content' ) ) {
-  function sek_print_shortcode_module_content( $shortcode_mod_html_content ) {
+  function sek_print_shortcode_module_content( $value = array() ) {
+    $shortcode_mod_html_content = $value['text_content'];
     // Use our own content filter instead of $content = apply_filters( 'the_content', $tiny_mce_content );
     // because of potential third party plugins corrupting 'the_content' filter. https://github.com/presscustomizr/nimble-builder/issues/233
     $content = apply_filters( 'the_nimble_tinymce_module_content', $shortcode_mod_html_content );
-    printf( '<div class="sek-shortcode-content">%1$s</div>', $content );
+    printf( '<div class="sek-shortcode-content" data-sek-use-flexbox="%2$s">%1$s</div>',
+      $content,
+      ( array_key_exists( 'use_flex', $value ) && true === sek_booleanize_checkbox_val( $value['use_flex'] ) ) ? "true" : "false"
+    );
   }
 }
 
-$model = Nimble_Manager()->model;
-$value = array_key_exists( 'value', $model ) ? $model['value'] : array();
-
 if ( !empty( $value['text_content'] ) ) {
-    sek_print_shortcode_module_content( $value['text_content'] );
+    sek_print_shortcode_module_content( $value );
 } else {
     if ( skp_is_customizing() ) {
         printf( '<div class="sek-mod-preview-placeholder"><div class="sek-preview-ph-text" style="%2$s"><p>%1$s</p></div></div>',
