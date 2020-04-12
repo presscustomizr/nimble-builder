9c61b8715d7be1866034456819ff284f7de1c87f
nikeo <nicolas@presscustomizr.com>
2020-04-12 18:17:31 +0200

    fixed : [module level] custom css class broken in the markup. fixes #659

--- inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
+++ inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
@@ -675,7 +675,7 @@ if ( ! class_exists( 'SEK_Front_Render' ) ) :
                       ?><script>nb_.emit('nb-needs-parallax');</script><?php
                     }
 
-                    printf('<div data-sek-level="module" data-sek-id="%1$s" data-sek-module-type="%2$s" class="sek-module %3$s %4$s" %5$s %6$s %7$s %8$s %9$s %10$s>%11$s',
+                    printf('<div data-sek-level="module" data-sek-id="%1$s" data-sek-module-type="%2$s" class="sek-module %3$s %4$s %5$s" %6$s %7$s %8$s %9$s %10$s>%11$s',
                         $id,
                         $module_type,
                         $this->get_level_visibility_css_class( $model ),
