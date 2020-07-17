783f6ff6b0be888d40441e6aa90276871b334079
nikeo <nicolas@presscustomizr.com>
2020-07-17 18:11:05 +0200

    first stone for #Note : July 2020 => function updated for https://github.com/presscustomizr/nimble-builder/issues/727

--- inc/sektions/_front_dev_php/dyn_css_builder_and_google_fonts_printer/5_0_2_class-sek-dyn-css-handler.php
+++ inc/sektions/_front_dev_php/dyn_css_builder_and_google_fonts_printer/5_0_2_class-sek-dyn-css-handler.php
@@ -293,12 +293,12 @@ class Sek_Dyn_CSS_Handler {
         // 4) front, user not logged in :
         //    the normal behaviour is that the css file is enqueued.
         //    It should have been written when saving in the customizer. If no file available, we try to write it. If writing a css file is not possible, we fallback on inline printing.
-        if ( is_customize_preview() || !$this->_sek_dyn_css_file_exists() || $this->force_rewrite || $this->customizer_save ) {
+        if ( is_customize_preview() || !$this->_sek_dyn_css_file_exists_is_readable_and_has_content() || $this->force_rewrite || $this->customizer_save ) {
             $this->sek_model = sek_get_skoped_seks( $this->skope_id );
 
             //  on front, when no stylesheet is available, the fallback hook must be set to wp_head, because the hook property might be empty
             // fixes https://github.com/presscustomizr/nimble-builder/issues/328
-            if ( !is_customize_preview() && !$this->_sek_dyn_css_file_exists() ) {
+            if ( !is_customize_preview() && !$this->_sek_dyn_css_file_exists_is_readable_and_has_content() ) {
                 $this->hook = 'wp_head';
             }
 
@@ -316,13 +316,17 @@ class Sek_Dyn_CSS_Handler {
         //hook setup for printing or enqueuing
         //bail if "customizer_save" == true, typically when saving the customizer settings @see Nimble_Customizer_Setting::update()
         if ( !$this->customizer_save ) {
-            $this->_schedule_css_and_fonts_enqueuing_or_printing_maybe_on_custom_hook();
+            if ( !empty($this->css_string_to_enqueue_or_print) ) {
+                $this->_schedule_css_and_fonts_enqueuing_or_printing_maybe_on_custom_hook();
+            } else {
+                $this->sek_dyn_css_maybe_delete_file_test();
+            }
         } else {
             //sek_error_log( __CLASS__ . '::' . __FUNCTION__ .' ?? => $this->css_string_to_enqueue_or_print => ', $this->css_string_to_enqueue_or_print );
-            if ( $this->css_string_to_enqueue_or_print ) {
+            if ( !empty($this->css_string_to_enqueue_or_print) ) {
                 $this->sek_dyn_css_maybe_write_css_file();
             } else {
-                $this->sek_dyn_css_maybe_delete_file();
+                $this->sek_dyn_css_maybe_delete_file_test();
             }
         }
     }//__construct
@@ -354,7 +358,7 @@ class Sek_Dyn_CSS_Handler {
         $this->uri                  = $this->_sek_dyn_css_build_uri();
         $this->url                  = $this->_ssl_maybe_fix_url( $this->_sek_dyn_css_build_url() );
 
-        $this->file_exists          = $this->_sek_dyn_css_file_exists();
+        $this->file_exists          = $this->_sek_dyn_css_file_exists_is_readable_and_has_content();
 
         if ( self::MODE_FILE == $this->mode ) {
             if ( !$this->_sek_dyn_css_write_file_is_possible() ) {
@@ -536,21 +540,18 @@ class Sek_Dyn_CSS_Handler {
     /**
      *
      * Remove the CSS file from the disk, if it exists
-     *
-     * @access public
+     * Note : July 2020 => function updated for https://github.com/presscustomizr/nimble-builder/issues/727
      *
      * @return bool TRUE if the CSS file has been deleted (or didn't exist already), FALSE otherwise
      */
     public function sek_dyn_css_maybe_delete_file() {
-        if ( $this->file_exists ) {
-            global $wp_filesystem;
+        global $wp_filesystem;
+        if ( $wp_filesystem->exists( $this->uri ) ) {
             $this->file_exists != $wp_filesystem->delete( $this->uri );
             return !$this->file_exists;
         }
         return !$this->file_exists;
     }
-
-
 
 
     /*
@@ -565,18 +566,26 @@ class Sek_Dyn_CSS_Handler {
      *
      * @return bool TRUE if the CSS file exists, FALSE otherwise
      */
-    private function _sek_dyn_css_file_exists() {
+    private function _sek_dyn_css_file_exists_is_readable_and_has_content() {
         global $wp_filesystem;
-        //sek_error_log( __CLASS__ . '::' . __FUNCTION__ . ' SOOO ? => ' . $this->uri . $wp_filesystem->exists( $this->uri ), empty( $file_content ) );
         if ( $wp_filesystem->exists( $this->uri ) ) {
             $file_content = $wp_filesystem->get_contents( $this->uri );
             return $wp_filesystem->is_readable( $this->uri ) && !empty( $file_content );
         } else {
             return false;
         }
-
     }
 
+    // Note : July 2020 => function introduced for https://github.com/presscustomizr/nimble-builder/issues/727
+    private function _sek_dyn_css_file_exists_and_is_empty() {
+        global $wp_filesystem;
+        if ( $wp_filesystem->exists( $this->uri ) ) {
+            $file_content = $wp_filesystem->get_contents( $this->uri );
+            return empty( $file_content );
+        } else {
+            return false;
+        }
+    }
 
 
     /**
