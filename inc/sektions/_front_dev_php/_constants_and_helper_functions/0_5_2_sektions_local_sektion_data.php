9212656bf14c1bc02b260b0a148533806fe4f417
nikeo <nicolas@presscustomizr.com>
2020-09-23 18:40:05 +0200

    added : [prebuild sections] a filter to add sections fixed : [prebuild sections][performances] removed the get sections action at 'nimble_front_classes_ready'

--- inc/sektions/_front_dev_php/_constants_and_helper_functions/0_5_2_sektions_local_sektion_data.php
+++ inc/sektions/_front_dev_php/_constants_and_helper_functions/0_5_2_sektions_local_sektion_data.php
@@ -55,7 +55,7 @@ function sek_get_raw_section_registration_params() {
                     'title' => __('3 call to action boxes, full-width background', 'text-domain' ),
                     'thumb' => 'intro_two.jpg',
                     'active' => sek_is_pro(),
-                    'is_pro' => !sek_is_pro()
+                    'is_pro' => true
                 )
             )
         ],
@@ -160,6 +160,8 @@ function sek_get_raw_section_registration_params() {
 
 /////////////////////////////////////////////////////////////
 // JSON FOR PRESET SECTIONS
+// update is forced every 24 hours, see transient : 'nimble_preset_sections_refreshed'
+// update is forced on 'upgrader_process_complete', on 'after_theme_switch'
 function sek_get_preset_section_collection_from_json( $force_update = false ) {
     // JULY 2020 => not stored in a transient anymore. For https://github.com/presscustomizr/nimble-builder/issues/730
     // + clean previously created transients
@@ -186,7 +188,8 @@ function sek_get_preset_section_collection_from_json( $force_update = false ) {
         // Save now as option for faster access next time
         update_option( NIMBLE_OPT_NAME_FOR_SECTION_JSON, $json_collection );
     }
-    return $json_collection;
+    // Filter used by NB Pro to add pro sections
+    return apply_filters( 'nimble_preset_sections_collection', $json_collection, $force_update );
 }
 
 
@@ -194,7 +197,7 @@ function sek_get_preset_section_collection_from_json( $force_update = false ) {
 // - theme switch
 // - nimble upgrade
 // - nimble is loaded ( only when is_admin() ) <= This makes the loading of the customizer faster on the first load, because the transient is ready.
-add_action( 'nimble_front_classes_ready', '\Nimble\sek_refresh_preset_sections_data');
+//add_action( 'nimble_front_classes_ready', '\Nimble\sek_refresh_preset_sections_data');
 add_action( 'after_switch_theme', '\Nimble\sek_refresh_preset_sections_data');
 add_action( 'upgrader_process_complete', '\Nimble\sek_refresh_preset_sections_data');
 function sek_refresh_preset_sections_data() {
