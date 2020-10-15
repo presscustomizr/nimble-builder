7aecebda5e7025dde9fe9b7bffbd76a3a519b5d9
nikeo <nicolas@presscustomizr.com>
2020-10-15 16:30:43 +0200

    v2.1.15 build

--- inc/sektions/ccat-sektions-front-modules.php
+++ inc/sektions/ccat-sektions-front-modules.php
@@ -4833,7 +4833,11 @@ function sek_get_module_params_for_czr_font_child() {
                     'refresh_markup' => false,
                     'refresh_stylesheet' => true,
                     'refresh_fonts' => true,
-                    'css_identifier' => 'font_family'
+                    'css_identifier' => 'font_family',
+                    'html_after' => sprintf('<span class="czr-notice"><i class="far fa-lightbulb"></i> %1s => <a href="%2s" target="_blank" rel="noopener noreferrer">%2$s</a></span><hr/>',
+                        __('Find inspiration'),
+                        'https://fonts.google.com/?sort=popularity'
+                    )
                 ),
                 'font_size_css'       => array(
                     'input_type'  => 'range_with_unit_picker_device_switcher',
