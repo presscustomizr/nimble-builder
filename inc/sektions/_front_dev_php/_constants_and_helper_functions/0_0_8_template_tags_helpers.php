c7b2caf79319b703c751e01d4413796dbd02b54e
nikeo <nicolas@presscustomizr.com>
2021-03-15 17:22:17 +0100

    added : [template tags] {{year_now}} to print the current year. {{site_title}} which prints the site title

--- inc/sektions/_front_dev_php/_constants_and_helper_functions/0_0_8_template_tags_helpers.php
+++ inc/sektions/_front_dev_php/_constants_and_helper_functions/0_0_8_template_tags_helpers.php
@@ -7,6 +7,8 @@
 function sek_find_pattern_match($matches) {
     $replace_values = apply_filters( 'sek_template_tags', array(
       'home_url' => 'home_url',
+      'year_now' => date("Y"),
+      'site_title' => 'get_bloginfo',
       'the_title' => 'sek_get_the_title',
       'the_content' => 'sek_get_the_content'
     ));
