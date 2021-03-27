634d4da0d3497153dfc702f33ec7ae3b1753345a
nikeo <nicolas@presscustomizr.com>
2021-03-27 17:12:45 +0100

    added : [site templates] new helper sek_has_group_site_template_data()

--- inc/sektions/_front_dev_php/_constants_and_helper_functions/0_9_0_1_sektions_functions_site_templates.php
+++ inc/sektions/_front_dev_php/_constants_and_helper_functions/0_9_0_1_sektions_functions_site_templates.php
@@ -109,6 +109,20 @@ function sek_get_group_site_template_data() {
 }
 
 
+// @return bool
+function sek_has_group_site_template_data() {
+    $cached = wp_cache_get('nimble_has_group_site_template_data');
+    if (  'yes' === $cached || 'no' === $cached ) {
+        return 'yes' === $cached;
+    }
+    
+    $group_site_tmpl_data = sek_get_group_site_template_data();//<= is cached when called
+    $has_group_skope_template_data = !( !$group_site_tmpl_data || empty($group_site_tmpl_data) );
+    sek_error_log('sek_has_group_site_template_data() ???', $has_group_skope_template_data );
+    wp_cache_set('nimble_has_group_site_template_data', $has_group_skope_template_data  ? 'yes' : 'no' );
+    return $has_group_skope_template_data;
+}
+
 
 /* ------------------------------------------------------------------------- *
  *  SITE TEMPLATES SAVE CUSTOMIZER ACTION
