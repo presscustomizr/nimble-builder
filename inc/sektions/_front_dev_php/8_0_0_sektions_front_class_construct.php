5634b9d47053e7042d5cd5981dbed27dc2a0cdca
nikeo <nicolas@presscustomizr.com>
2021-04-15 19:01:05 +0200

    improved : PHP remove properties ::local_seks and ::global_seks, now replaced by WP core cache system

--- inc/sektions/_front_dev_php/8_0_0_sektions_front_class_construct.php
+++ inc/sektions/_front_dev_php/8_0_0_sektions_front_class_construct.php
@@ -4,8 +4,6 @@
 if ( !class_exists( 'SEK_Front_Construct' ) ) :
     class SEK_Front_Construct {
         static $instance;
-        public $local_seks = 'not_cached';// <= used to cache the sektions for the local skope_id
-        public $global_seks = 'not_cached';// <= used to cache the sektions for the global skope_id
         public $seks_posts = [];// <= march 2020 : used to cache the current local and global sektion posts
         public $model = array();//<= when rendering, the current level model
         public $parent_model = array();//<= when rendering, the current parent model
