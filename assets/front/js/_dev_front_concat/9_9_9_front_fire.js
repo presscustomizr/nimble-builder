presscustomizr/nimble-builder

commit 864411098cc3c980665349d751ff03940b35bf04

improved : [post grid module] clean "go_to" param in url when navigating the post pagination

assets/front/js/_dev_front_concat/9_9_9_front_fire.js
@@ -154,11 +154,23 @@
 /* ------------------------------------------------------------------------- */
 // June 2020 : added for https://github.com/presscustomizr/nimble-builder/issues/716
 nb_.listenTo('nb-docready', function() {
-    if ( window.nb_ && window.nb_.getQueryVariable ) {
-        var anchorId = window.nb_.getQueryVariable('go_to'),
-            el = document.getElementById(anchorId);
-        if( anchorId && el ) {
-              setTimeout( function() { el.scrollIntoView();}, 200 );
-        }
-    }
+      if ( window.nb_ && window.nb_.getQueryVariable ) {
+            var anchorId = window.nb_.getQueryVariable('go_to'),
+                  el = document.getElementById(anchorId);
+            if( anchorId && el ) {
+                  setTimeout( function() { el.scrollIntoView();}, 200 );
+            }
+            // Then clean the url
+            var _cleanUrl = function() {
+                  var currPathName = window.location.pathname; //get current address
+                  //1- get the part before '?go_to'
+                  var beforeQueryString = currPathName.split("?go_to")[0];
+                  window.history.replaceState({}, document.title,  beforeQueryString );
+            };
+            try{ _cleanUrl(); } catch(er) {
+                  if( window.console && window.console.log ) {
+                        console.log( 'NB => error when cleaning url "go_to" param');
+                  }
+            }
+      }
 });

commit 7425c9fda389d6050298ea7f97da5a7c9a03e823

fixed : bug when trying to edit a draft or scheduled post. regression introduced in https://github.com/presscustomizr/nimble-builder/commit/864411098cc3c980665349d751ff03940b35bf04 fixes #834

assets/front/js/_dev_front_concat/9_9_9_front_fire.js
@@ -157,9 +157,6 @@ nb_.listenTo('nb-docready', function() {
       if ( window.nb_ && window.nb_.getQueryVariable ) {
             var anchorId = window.nb_.getQueryVariable('go_to'),
                   el = document.getElementById(anchorId);
-            if( anchorId && el ) {
-                  setTimeout( function() { el.scrollIntoView();}, 200 );
-            }
             // Then clean the url
             var _cleanUrl = function() {
                   var currPathName = window.location.pathname; //get current address
@@ -167,9 +164,12 @@ nb_.listenTo('nb-docready', function() {
                   var beforeQueryString = currPathName.split("?go_to")[0];
                   window.history.replaceState({}, document.title,  beforeQueryString );
             };
-            try{ _cleanUrl(); } catch(er) {
-                  if( window.console && window.console.log ) {
-                        console.log( 'NB => error when cleaning url "go_to" param');
+            if( anchorId && el ) {
+                  setTimeout( function() { el.scrollIntoView();}, 200 );
+                  try{ _cleanUrl(); } catch(er) {
+                        if( window.console && window.console.log ) {
+                              console.log( 'NB => error when cleaning url "go_to" param');
+                        }
                   }
             }
       }