346481955da67157fa1aa97d84509a46e67a847d
nikeo <nicolas@presscustomizr.com>
2021-06-22 16:01:17 +0200

    fixed : [version in body tag classes] make sure both versions of nb pro and nb free are printed fixes https://github.com/presscustomizr/nimble-builder-pro/issues/206

--- inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
+++ inc/sektions/_front_dev_php/8_4_0_sektions_front_class_render.php
@@ -74,8 +74,10 @@ if ( !class_exists( 'SEK_Front_Render' ) ) :
                 }
                 array_unshift( $classes, !sek_local_skope_has_been_customized() ? 'nimble-no-local-data-' . $skope_id : 'nimble-has-local-data-' . $skope_id );
             }
-
-            array_unshift( $classes, sek_is_pro() ? 'nimble-builder-pro-' . str_replace('.', '-', NB_PRO_VERSION ) : 'nimble-builder-' . str_replace('.', '-', NIMBLE_VERSION ) );
+            if ( sek_is_pro() ) {
+                array_unshift( $classes, 'nimble-builder-pro-' . str_replace('.', '-', NB_PRO_VERSION ) );
+            }
+            array_unshift( $classes, 'nimble-builder-' . str_replace('.', '-', NIMBLE_VERSION ) );
 
             return $classes;
         }
