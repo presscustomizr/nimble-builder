faaff4932bbf45fc541b534d1a24d7251d3c7ba5
nikeo <nicolas@presscustomizr.com>
2021-09-14 11:21:10 +0200

    fixed : [php] replaced ob_get_contents() by ob_get_clean()

--- inc/admin/nimble-admin.php
+++ inc/admin/nimble-admin.php
@@ -729,8 +729,7 @@ function sek_may_be_display_update_notice() {
         </p> -->
       </div>
       <?php
-      $_html = ob_get_contents();
-      if ($_html) ob_end_clean();
+      $_html = ob_get_clean();
       echo apply_filters( 'sek_update_notice', $_html );
       ?>
       <script type="text/javascript" id="nimble-dismiss-update-notice">
@@ -860,8 +859,7 @@ function sek_maybe_display_feedback_notice() {
       </button>
     </div>
     <?php
-      $_html = ob_get_contents();
-      if ($_html) ob_end_clean();
+      $_html = ob_get_clean();
       echo apply_filters( 'sek_feedback_notice', $_html );
     ?>
     <script>
