6c74465f5c6573b961b1fb92333c27a628ae401e
nikeo <nicolas@presscustomizr.com>
2022-04-01 19:38:34 +0200

    fixed typo

--- tmpl/wp/comments-template.php
+++ tmpl/wp/comments-template.php
@@ -3,7 +3,7 @@
 <section id="sek-comments" class="sek-themeform">
 	<?php if ( have_comments() ) : global $wp_query; ?>
 
-		<h3><?php comments_number( __( 'No Responses', 'hueman' ), __( '1 Response', 'hueman' ), __( '% Responses', 'hueman' ) ); ?></h3>
+		<h3><?php comments_number( __( 'No Responses', 'text_doma' ), __( '1 Response', 'text_doma' ), __( '% Responses', 'text_doma' ) ); ?></h3>
 
 		<div id="sek-commentlist-container" class="comment-tab">
 
